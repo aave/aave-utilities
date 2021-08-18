bf9997ae9136419494488ad57d49307988003af2
sakulstra <lukasstrassel@googlemail.com>
2021-08-18 09:57:46 +0200

    fix: remove failing test

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -6,9 +6,5 @@ export function foo() {
 	return 'bar'
 }
 
-export function test() {
-	return 'bla'
-}
-
 // For CommonJS default export support
 module.exports = Object.assign(exports.default, exports)
