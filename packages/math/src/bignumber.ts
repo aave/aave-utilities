7fd4ef134ad1af5315c8c09f129d6faa2aeff9a8
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-08-20 11:23:07 +0100

    Unit tests for some maths logic (#15)
    * feat: initial coding tests written
    
    * feat: unit tests for math code
    
    * feature: removed `valueToZDBigNumber` unit test
    
    * move prettier
    
    * fix: eslint error
    
    * fix: disable `no-negated-condition`
    
    * fix: temp add to ignore some coverage of math files
    
    * fix: pr comments

---
+++ packages/math/src/bignumber.ts
@@ -0,0 +1,24 @@
+import { BigNumber } from 'bignumber.js'
+
+export type BigNumberValue = string | number | BigNumber
+
+export const BigNumberZeroDecimal = BigNumber.clone({
+	DECIMAL_PLACES: 0,
+	ROUNDING_MODE: BigNumber.ROUND_DOWN,
+})
+
+export function valueToBigNumber(amount: BigNumberValue): BigNumber {
+	return new BigNumber(amount)
+}
+
+export function valueToZDBigNumber(amount: BigNumberValue): BigNumber {
+	return new BigNumberZeroDecimal(amount)
+}
+
+export function normalize(n: BigNumberValue, decimals: number): string {
+	return normalizeBN(n, decimals).toString(10)
+}
+
+export function normalizeBN(n: BigNumberValue, decimals: number): BigNumber {
+	return valueToBigNumber(n).shiftedBy(decimals * -1)
+}
