0861b9d09e3c15cd1d2ba453d01ff06ef9aaa558
Josh Stevens <joshstevens19@hotmail.co.uk>
2021-09-02 12:50:01 +0100

    feat: ui incentive data provider

---
+++ packages/contract-helpers/src/ui-incentive-data-provider/index.ts
@@ -0,0 +1,78 @@
+import { ethers, providers } from 'ethers';
+import { isAddress } from 'ethers/lib/utils';
+import { abi } from './abi';
+import {
+  AggregatedreserveincentivedataResponse,
+  ContractContext,
+  GetFullReservesIncentiveDataResponse,
+  UserreserveincentivedataResponse,
+} from './types/UiIncentiveDataProvider';
+
+export {
+  AggregatedreserveincentivedataResponse,
+  GetFullReservesIncentiveDataResponse,
+  UserreserveincentivedataResponse,
+} from './types/UiIncentiveDataProvider';
+
+export class UiIncentiveDataProvider {
+  private readonly _contract: ContractContext;
+
+  private readonly _lendingPoolAddress: string;
+
+  public constructor(
+    contractAddress: string,
+    lendingPoolAddress: string,
+    provider: providers.Provider,
+  ) {
+    if (!isAddress(lendingPoolAddress)) {
+      throw new Error('Lending pool address is not valid');
+    }
+
+    this._lendingPoolAddress = lendingPoolAddress;
+
+    this._contract = (new ethers.Contract(
+      contractAddress,
+      abi,
+      provider,
+    ) as unknown) as ContractContext;
+  }
+
+  /**
+   *  Get the full reserve incentive data for the lending pool and the user
+   * @param user The user address
+   */
+  public async getFullReserves(
+    user: string,
+  ): Promise<GetFullReservesIncentiveDataResponse> {
+    if (!isAddress(user)) {
+      throw new Error('User address is not a valid ethereum address');
+    }
+
+    return this._contract.getFullReservesIncentiveData(
+      this._lendingPoolAddress,
+      user,
+    );
+  }
+
+  /**
+   *  Get the reserve incentive data for the lending pool
+   */
+  public async getReserves(): Promise<
+    AggregatedreserveincentivedataResponse[]
+  > {
+    return this._contract.getReservesIncentivesData(this._lendingPoolAddress);
+  }
+
+  /**
+   *  Get the reserve incentive data for the user
+   * @param user The user address
+   */
+  public async getUserReserves(
+    user: string,
+  ): Promise<UserreserveincentivedataResponse[]> {
+    return this._contract.getUserReservesIncentivesData(
+      this._lendingPoolAddress,
+      user,
+    );
+  }
+}
