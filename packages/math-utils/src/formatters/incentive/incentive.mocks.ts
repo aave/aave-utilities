a1dda324663c81d5549f7be5d2dc83386256cb2e
Andrew Schmidt <andrew@aave.com>
2021-09-20 20:39:06 -0500

    feat: update mock inputs for user reserve incentives

---
+++ packages/math-utils/src/formatters/incentive/incentive.mocks.ts
@@ -0,0 +1,292 @@
+import BigNumber from 'bignumber.js';
+
+// Reserve Incentives
+
+export const aETHReserveIncentiveData = {
+  underlyingAsset: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  aIncentiveData: {
+    emissionPerSecond: new BigNumber(1979166666666666),
+    incentivesLastUpdateTimestamp: new BigNumber(1631587511),
+    tokenIncentivesIndex: new BigNumber(24733519699535219),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  vIncentiveData: {
+    emissionPerSecond: new BigNumber(104166666666666),
+    incentivesLastUpdateTimestamp: new BigNumber(1631587387),
+    tokenIncentivesIndex: new BigNumber(26465727412280876),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  sIncentiveData: {
+    emissionPerSecond: new BigNumber(0),
+    incentivesLastUpdateTimestamp: new BigNumber(0),
+    tokenIncentivesIndex: new BigNumber(0),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+};
+
+export const aUSDCReserveIncentiveData = {
+  underlyingAsset: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  aIncentiveData: {
+    emissionPerSecond: new BigNumber(4629629629629629),
+    incentivesLastUpdateTimestamp: new BigNumber(1631586625),
+    tokenIncentivesIndex: new BigNumber(16055272805126608426463068),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  vIncentiveData: {
+    emissionPerSecond: new BigNumber(4629629629629629),
+    incentivesLastUpdateTimestamp: new BigNumber(1631586625),
+    tokenIncentivesIndex: new BigNumber(21239260963515875537848724),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  sIncentiveData: {
+    emissionPerSecond: new BigNumber(0),
+    incentivesLastUpdateTimestamp: new BigNumber(0),
+    tokenIncentivesIndex: new BigNumber(0),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+};
+
+export const aXSUSHIReserveIncentiveData = {
+  underlyingAsset: '0x8798249c2e607446efb7ad49ec89dd1865ff4272',
+  aIncentiveData: {
+    emissionPerSecond: new BigNumber(173611111111111),
+    incentivesLastUpdateTimestamp: new BigNumber(1631586256),
+    tokenIncentivesIndex: new BigNumber(23156815865521),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  vIncentiveData: {
+    emissionPerSecond: new BigNumber(0),
+    incentivesLastUpdateTimestamp: new BigNumber(1629797428),
+    tokenIncentivesIndex: new BigNumber(0),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  sIncentiveData: {
+    emissionPerSecond: new BigNumber(0),
+    incentivesLastUpdateTimestamp: new BigNumber(0),
+    tokenIncentivesIndex: new BigNumber(0),
+    emissionEndTimestamp: new BigNumber(1637573428),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+};
+
+// User Incentives
+
+export const aETHUserIncentiveData = {
+  underlyingAsset: '0x030ba81f1c18d280636f32af80b9aad02cf0854e',
+  aIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  vIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(24934844000963410),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  sIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+};
+
+export const aUSDCUserIncentiveData = {
+  underlyingAsset: '0x030ba81f1c18d280636f32af80b9aad02cf0854e',
+  aIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(8399742855606485876888576),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  vIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  sIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+};
+
+export const aXSUSHIUserIncentiveData = {
+  underlyingAsset: '0x8798249c2e607446efb7ad49ec89dd1865ff4272',
+  aIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  vIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+  sIncentiveData: {
+    tokenIncentivesUserIndex: new BigNumber(0),
+    userUnclaimedRewards: new BigNumber(43921819137644870),
+    tokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenDecimals: 18,
+  },
+};
+
+// Reserve Data
+
+export const aETHReserveData = {
+  underlyingAsset: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
+  symbol: 'aWETH',
+  decimals: 18,
+  baseLTVasCollateral: '8000',
+  reserveLiquidationThreshold: '8500',
+  reserveLiquidationBonus: '10500',
+  reserveFactor: '1000',
+  usageAsCollateralEnabled: true,
+  borrowingEnabled: true,
+  stableBorrowRateEnabled: true,
+  isActive: true,
+  liquidityIndex: '1007431539067282236768346040',
+  variableBorrowIndex: '1009130500436609696185241835',
+  liquidityRate: '222438954027153387451224',
+  variableBorrowRate: '5352853290785089356838046',
+  stableBorrowRate: '36691066613481361696047558',
+  lastUpdateTimestamp: 1631587511,
+  aTokenAddress: '0x030ba81f1c18d280636f32af80b9aad02cf0854e',
+  stableDebtTokenAddress: '0x4e977830ba4bd783c0bb7f15d3e243f73ff57121',
+  variableDebtTokenAddress: '0xf63b34710400cad3e044cffdcab00a0f32e33ecf',
+  interestRateStrategyAddress: '0x4ce076b9dd956196b814e54e1714338f18fde3f4',
+  availableLiquidity: '1827977661703998535260749',
+  totalPrincipalStableDebt: '681322787738991248642',
+  averageStableRate: '45598025477532527825795055',
+  stableDebtLastUpdateTimestamp: 1631554490,
+  totalScaledVariableDebt: '81689984341288838884434',
+  priceInMarketReferenceCurrency: '1000000000000000000',
+  variableRateSlope1: '80000000000000000000000000',
+  variableRateSlope2: '1000000000000000000000000000',
+  stableRateSlope1: '100000000000000000000000000',
+  stableRateSlope2: '1000000000000000000000000000',
+};
+
+export const aUSDCReserveData = {
+  underlyingAsset: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+  symbol: 'aUSDC',
+  decimals: 6,
+  baseLTVasCollateral: '8250',
+  reserveLiquidationThreshold: '8500',
+  reserveLiquidationBonus: '10500',
+  reserveFactor: '1000',
+  usageAsCollateralEnabled: true,
+  borrowingEnabled: true,
+  stableBorrowRateEnabled: true,
+  isActive: true,
+  liquidityIndex: '1051634606451872054462648399',
+  variableBorrowIndex: '1074617773121094489748370739',
+  liquidityRate: '71570760834786680706411322',
+  variableBorrowRate: '87363273409228566120889888',
+  stableBorrowRate: '157363273409228566120889888',
+  lastUpdateTimestamp: 1631586625,
+  aTokenAddress: '0xbcca60bb61934080951369a648fb03df4f96263c',
+  stableDebtTokenAddress: '0xe4922afab0bbadd8ab2a88e0c79d884ad337fca6',
+  variableDebtTokenAddress: '0x619beb58998ed2278e08620f97007e1116d5d25b',
+  interestRateStrategyAddress: '0x8cae0596bc1ed42dc3f04c4506cfe442b3e74e27',
+  availableLiquidity: '530892058938890',
+  totalPrincipalStableDebt: '42316057119911',
+  averageStableRate: '115488581341600315852026679',
+  stableDebtLastUpdateTimestamp: 1631582374,
+  totalScaledVariableDebt: '4830283465422516',
+  priceInMarketReferenceCurrency: '303670332634581',
+  variableRateSlope1: '40000000000000000000000000',
+  variableRateSlope2: '600000000000000000000000000',
+  stableRateSlope1: '20000000000000000000000000',
+  stableRateSlope2: '600000000000000000000000000',
+};
+
+export const aXSUSHIReserveData = {
+  underlyingAsset: '0x8798249c2e607446efb7ad49ec89dd1865ff4272',
+  symbol: 'aXSUSHI',
+  decimals: 18,
+  baseLTVasCollateral: '3500',
+  reserveLiquidationThreshold: '6000',
+  reserveLiquidationBonus: '11250',
+  reserveFactor: '3500',
+  usageAsCollateralEnabled: true,
+  borrowingEnabled: true,
+  isActive: true,
+  liquidityIndex: '1000123598083549765972697918',
+  variableBorrowIndex: '1002094270517252837393825417',
+  liquidityRate: '989354704923652458659',
+  variableBorrowRate: '486588786689701825019956',
+  stableBorrowRate: '0',
+  lastUpdateTimestamp: 1631583392,
+  aTokenAddress: '0xf256cc7847e919fac9b808cc216cac87ccf2f47a',
+  stableDebtTokenAddress: '0x73bfb81d7dba75c904f430ea8bae82db0d41187b',
+  variableDebtTokenAddress: '0xfafedf95e21184e3d880bd56d4806c4b8d31c69a',
+  interestRateStrategyAddress: '0xb49034ada4be5c6bb3823a623c6250267110b06b',
+  availableLiquidity: '12788417052387844754293422',
+  totalPrincipalStableDebt: '0',
+  averageStableRate: '0',
+  stableDebtLastUpdateTimestamp: 0,
+  totalScaledVariableDebt: '40044734186804090188243',
+  priceInMarketReferenceCurrency: '3957308197441592',
+  variableRateSlope1: '70000000000000000000000000',
+  variableRateSlope2: '3000000000000000000000000000',
+  stableRateSlope1: '0',
+  stableRateSlope2: '0',
+};
+
+// User Reserve Data
+
+export const aETHScaledATokenBalance = new BigNumber('99353924118371338');
+export const aETHScaledVariableDebt = new BigNumber('0');
+export const aETHPrincipalStableDebt = new BigNumber('0');
+
+export const aUSDCScaledATokenBalance = new BigNumber('2328085146');
+export const aUSDCScaledVariableDebt = new BigNumber('48856783');
+export const aUSDCPrincipalStableDebt = new BigNumber('0');
+
+export const aXSUSHIScaledATokenBalance = new BigNumber(
+  '1031991722452535126997',
+);
+export const aXSUSHIScaledVariableDebt = new BigNumber('0');
+export const aXSUSHIPrincipalStableDebt = new BigNumber('0');
