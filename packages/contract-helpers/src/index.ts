0ecb0bf62f3cd0084f6b7ee4e0c6151f68c646a0
sakulstra <lukasstrassel@googlemail.com>
2021-09-29 15:24:06 +0200

    fix: also export types

--- packages/contract-helpers/src/index.ts
+++ packages/contract-helpers/src/index.ts
@@ -1,4 +1,8 @@
 export * from './permissions-manager';
+export {
+  PERMISSION,
+  PERMISSION_MAP,
+} from './permissions-manager/types/PermissionManagerTypes';
 export * from './ui-incentive-data-provider';
 export * from './ui-pool-data-provider';
 export * from './wallet-balance-provider';
