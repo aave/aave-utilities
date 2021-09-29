7931421e2844c631db2abef65dd859a7edc87335
Lukas Strassel <lukasstrassel@googlemail.com>
2021-09-29 21:08:53 +0200

    feat: permissions manager (#73)
    * feat: add permissions manager
    
    * fix: increase coverage
    
    * fix: test
    
    * fix: improve converage
    
    * fix: also export types

---
+++ packages/contract-helpers/src/permissions-manager/index.test.ts
@@ -0,0 +1,64 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { BigNumber, providers } from 'ethers';
+import { getUserPermissionsResponseMock } from './_mocks';
+import { PERMISSION } from './types/PermissionManagerTypes';
+import { PermissionManager } from './index';
+
+const mockInvalidEthereumAddress = '0x0';
+const mockValidEthereumAddress = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984';
+
+describe('PermissionManager', () => {
+  describe('creating', () => {
+    it('should throw an error if the contractAddress is not valid', () => {
+      expect(
+        () =>
+          new PermissionManager({
+            permissionManagerAddress: mockInvalidEthereumAddress,
+            provider: new providers.JsonRpcProvider(),
+          }),
+      ).toThrowError('contract address is not valid');
+    });
+  });
+
+  describe('getHumanizedUserPermissions', () => {
+    const instance = new PermissionManager({
+      permissionManagerAddress: mockValidEthereumAddress,
+      provider: new providers.JsonRpcProvider(),
+    });
+
+    const mock = jest.fn();
+    // @ts-ignore
+    instance._contract = {
+      getUserPermissions: mock,
+    };
+
+    it('should throw an error if the user is not valid', async () => {
+      await expect(
+        instance.getHumanizedUserPermissions(mockInvalidEthereumAddress),
+      ).rejects.toThrow('User address is not a valid ethereum address');
+    });
+
+    it('should throw an error if the permission is not known', async () => {
+      mock.mockResolvedValueOnce({
+        0: [BigNumber.from(5), ...getUserPermissionsResponseMock[0]],
+        1: getUserPermissionsResponseMock[1],
+      });
+      await expect(
+        instance.getHumanizedUserPermissions(mockValidEthereumAddress),
+      ).rejects.toThrow('Error parsing permission');
+    });
+
+    it('should return human readable response', async () => {
+      mock.mockResolvedValueOnce(getUserPermissionsResponseMock);
+
+      const response = await instance.getHumanizedUserPermissions(
+        mockValidEthereumAddress,
+      );
+
+      expect(response).toStrictEqual([
+        PERMISSION.DEPOSITOR,
+        PERMISSION.BORROWER,
+      ]);
+    });
+  });
+});
---
+++ packages/contract-helpers/src/permissions-manager/index.ts
@@ -0,0 +1,57 @@
+/* eslint-disable @typescript-eslint/require-await */
+import { BigNumber, providers } from 'ethers';
+import { isAddress } from 'ethers/lib/utils';
+import { PermissionManager as PermissionManagerContract } from './typechain/PermissionManager';
+import { PermissionManagerFactory } from './typechain/PermissionManagerFactory';
+import { PERMISSION, PERMISSION_MAP } from './types/PermissionManagerTypes';
+
+export interface PermissionManagerContext {
+  permissionManagerAddress: string;
+  provider: providers.Provider;
+}
+
+export class PermissionManager {
+  private readonly _contract: PermissionManagerContract;
+
+  public constructor(context: PermissionManagerContext) {
+    if (!isAddress(context.permissionManagerAddress)) {
+      throw new Error('contract address is not valid');
+    }
+
+    this._contract = PermissionManagerFactory.connect(
+      context.permissionManagerAddress,
+      context.provider,
+    );
+  }
+
+  public async getUserPermissions(
+    user: string,
+  ): Promise<{ 0: BigNumber[]; 1: BigNumber }> {
+    if (!isAddress(user)) {
+      throw new Error('User address is not a valid ethereum address');
+    }
+
+    return this._contract.getUserPermissions(user);
+  }
+
+  public async getHumanizedUserPermissions(
+    user: string,
+  ): Promise<PERMISSION[]> {
+    const {
+      0: permissions,
+      1: permissionsNumber,
+    } = await this.getUserPermissions(user);
+
+    const parsedPermissions: PERMISSION[] = [];
+    permissions.forEach((permission: BigNumber, index: number) => {
+      if (index < permissionsNumber.toNumber()) {
+        const permissionIndex = permission.toNumber();
+        if (permissionIndex < 0 || permissionIndex > 4)
+          throw new Error('Error parsing permission');
+        parsedPermissions[index] =
+          PERMISSION_MAP[permissionIndex as 0 | 1 | 2 | 3];
+      }
+    });
+    return parsedPermissions;
+  }
+}
