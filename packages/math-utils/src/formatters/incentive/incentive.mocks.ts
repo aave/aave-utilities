f04547b6b2cbf1e062cb8edc61fa27621de3b8a9
Andrew Schmidt <andrew@aave.com>
2021-09-30 08:06:00 -0500

    fix: user incentives input type and export response type

--- packages/math-utils/src/formatters/incentive/incentive.mocks.ts
+++ packages/math-utils/src/formatters/incentive/incentive.mocks.ts
@@ -243,9 +243,6 @@ export const userReserveIncentives = [
 
 export const aETHReserve = {
   underlyingAsset: '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2',
-  aTokenAddress: '0x0000000000000000000000000000000000000001',
-  variableDebtTokenAddress: '0x0000000000000000000000000000000000000002',
-  stableDebtTokenAddress: '0x0000000000000000000000000000000000000003',
   totalLiquidity: '2003772886310189627431436',
   liquidityIndex: '1007431539067282236768346040',
   totalScaledVariableDebt: '81689984341288838884434',
@@ -256,9 +253,6 @@ export const aETHReserve = {
 };
 export const aUSDCReserve = {
   underlyingAsset: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-  aTokenAddress: '0x0000000000000000000000000000000000000004',
-  variableDebtTokenAddress: '0x0000000000000000000000000000000000000005',
-  stableDebtTokenAddress: '0x0000000000000000000000000000000000000006',
   totalLiquidity: '5787718415383817',
   liquidityIndex: '1051634606451872054462648399',
   totalScaledVariableDebt: '4830283465422516',
@@ -269,9 +263,6 @@ export const aUSDCReserve = {
 };
 export const aXSUSHIReserve = {
   underlyingAsset: '0x8798249c2e607446efb7ad49ec89dd1865ff4272',
-  aTokenAddress: '0x0000000000000000000000000000000000000007',
-  variableDebtTokenAddress: '0x0000000000000000000000000000000000000008',
-  stableDebtTokenAddress: '0x0000000000000000000000000000000000000009',
   totalLiquidity: '12633368340656382491578069',
   liquidityIndex: '1000123598083549765972697918',
   totalScaledVariableDebt: '40044734186804090188243',
