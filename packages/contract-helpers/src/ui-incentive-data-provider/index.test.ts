43ed8d0ee5cc2812d40b73e4b88fcbdc4ce1becb
sendra <polsendra@gmail.com>
2021-10-06 12:37:23 +0200

    feat: added tests

--- packages/contract-helpers/src/ui-incentive-data-provider/index.test.ts
+++ packages/contract-helpers/src/ui-incentive-data-provider/index.test.ts
@@ -1,6 +1,16 @@
 import { providers } from 'ethers';
+import { mocked } from 'ts-jest/utils';
+import { ChainlinkFeedsRegistry } from '../cl-feed-registry/index';
+import { Denominations } from '../cl-feed-registry/types/ChainlinkFeedsRegistryTypes';
 import { getReservesIncentivesDataMock } from './_mocks';
-import { UiIncentiveDataProvider } from './index';
+import { ReserveIncentiveWithFeedsResponse } from './types/UiIncentiveDataProviderTypes';
+import UiIncentiveDataProvider from './index';
+
+jest.mock('../cl-feed-registry/index', () => {
+  const clInstance = { getPriceFeed: jest.fn() };
+  const cl = jest.fn(() => clInstance);
+  return { ChainlinkFeedsRegistry: cl };
+});
 
 describe('UiIncentiveDataProvider', () => {
   const mockValidEthereumAddress = '0x1f9840a85d5af5bf1d1762f925bdaddc4201f984';
@@ -109,19 +119,99 @@ describe('UiIncentiveDataProvider', () => {
   });
 
   describe('getReservesIncentivesDataHumanized', () => {
+    it('Should throw error if token address is wrong', async () => {
+      const instance = createValidInstance();
+      await expect(
+        instance.getReservesIncentivesDataHumanized(mockInvalidEthereumAddress),
+      ).rejects.toThrow('Lending pool address provider is not valid');
+    });
+  });
+
+  describe('getReservesIncentivesDataHumanized', () => {
+    it('Should throw error if token address is wrong', async () => {
+      const instance = createValidInstance();
+      await expect(
+        instance.getReservesIncentivesDataHumanized(mockInvalidEthereumAddress),
+      ).rejects.toThrow('Lending pool address provider is not valid');
+    });
     it('should not throw', async () => {
+      const clInstance = new ChainlinkFeedsRegistry({
+        chainlinkFeedsRegistry: mockValidEthereumAddress,
+        provider: new providers.JsonRpcProvider(),
+      });
       const instance = createValidInstance();
 
-      let errored = false;
-      try {
-        await instance.getReservesIncentivesDataHumanized(
-          mockValidEthereumAddress,
-        );
-      } catch (_) {
-        errored = true;
-      }
+      // eslint-disable-next-line @typescript-eslint/require-await
+      mocked(clInstance).getPriceFeed.mockReturnValueOnce(Promise.reject());
+      // eslint-disable-next-line @typescript-eslint/require-await
+      mocked(clInstance).getPriceFeed.mockReturnValue(
+        Promise.resolve({
+          answer: '2',
+          updatedAt: 4,
+          decimals: 1,
+        }),
+      );
 
-      expect(errored).toEqual(false);
+      const result: ReserveIncentiveWithFeedsResponse[] = await instance.getIncentivesDataWithPrice(
+        mockValidEthereumAddress,
+        mockValidEthereumAddress,
+        Denominations.eth,
+      );
+
+      expect(clInstance.getPriceFeed).toBeCalled();
+      expect(typeof result[0].aIncentiveData.emissionEndTimestamp).toEqual(
+        typeof 1,
+      );
+      expect(result).toEqual([
+        {
+          underlyingAsset: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+          aIncentiveData: {
+            tokenAddress: '0x3Ed3B47Dd13EC9a98b44e6204A523E766B225811',
+            precision: 18,
+            rewardTokenAddress: '0x4da27a545c0c5B758a6BA100e3a049001de870f5',
+            incentiveControllerAddress:
+              '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
+            rewardTokenDecimals: 18,
+            emissionPerSecond: '1880787037037037',
+            incentivesLastUpdateTimestamp: 1633175478,
+            tokenIncentivesIndex: '43565143328112327495233486',
+            emissionEndTimestamp: 1637573428,
+            priceFeed: '0',
+            priceFeedTimestamp: 0,
+            priceFeedDecimals: 0,
+          },
+          vIncentiveData: {
+            tokenAddress: '0x531842cEbbdD378f8ee36D171d6cC9C4fcf475Ec',
+            precision: 18,
+            rewardTokenAddress: '0x4da27a545c0c5B758a6BA100e3a049001de870f5',
+            incentiveControllerAddress:
+              '0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5',
+            rewardTokenDecimals: 18,
+            emissionPerSecond: '1880787037037037',
+            incentivesLastUpdateTimestamp: 1633175478,
+            tokenIncentivesIndex: '57970476598005880594044681',
+            emissionEndTimestamp: 1637573428,
+            priceFeed: '0',
+            priceFeedTimestamp: 0,
+            priceFeedDecimals: 0,
+          },
+          sIncentiveData: {
+            tokenAddress: '0x0000000000000000000000000000000000000000',
+            precision: 0,
+            rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+            incentiveControllerAddress:
+              '0x0000000000000000000000000000000000000000',
+            rewardTokenDecimals: 0,
+            emissionPerSecond: '0',
+            incentivesLastUpdateTimestamp: 0,
+            tokenIncentivesIndex: '0',
+            emissionEndTimestamp: 0,
+            priceFeed: '2',
+            priceFeedTimestamp: 4,
+            priceFeedDecimals: 1,
+          },
+        },
+      ]);
     });
   });
 
--- packages/contract-helpers/src/ui-incentive-data-provider/index.ts
+++ packages/contract-helpers/src/ui-incentive-data-provider/index.ts
@@ -1,13 +1,24 @@
+/* eslint-disable require-atomic-updates */
 import { providers } from 'ethers';
 import { isAddress } from 'ethers/lib/utils';
+import {
+  ChainlinkFeedsRegistry,
+  ChainlinkFeedsRegistryInterface,
+  Denominations,
+  PriceFeed,
+} from '../cl-feed-registry/index';
 import { UiIncentiveDataProvider as UiIncentiveDataProviderContract } from './typechain/UiIncentiveDataProvider';
 import { UiIncentiveDataProviderFactory } from './typechain/UiIncentiveDataProviderFactory';
 import {
   FullReservesIncentiveDataResponse,
   IncentiveData,
   IncentiveDataHumanized,
+  IncentiveUserData,
+  IncentiveUserDataHumanized,
   ReserveIncentiveDataHumanizedResponse,
   ReserveIncentiveDataResponse,
+  ReserveIncentiveWithFeedsResponse,
+  UserReserveIncentiveDataHumanizedResponse,
   UserReserveIncentiveDataResponse,
 } from './types/UiIncentiveDataProviderTypes';
 export * from './types/UiIncentiveDataProviderTypes';
@@ -30,9 +41,16 @@ export interface UiIncentiveDataProviderContext {
   incentiveDataProviderAddress: string;
   provider: providers.Provider;
 }
-export class UiIncentiveDataProvider
+
+export default class UiIncentiveDataProvider
   implements UiIncentiveDataProviderInterface {
   public readonly _contract: UiIncentiveDataProviderContract;
+
+  private readonly _chainlinkFeedsRegistries: {
+    [address: string]: ChainlinkFeedsRegistryInterface;
+  };
+
+  private readonly _context: UiIncentiveDataProviderContext;
 
   /**
    * Constructor
@@ -42,6 +60,10 @@ export class UiIncentiveDataProvider
     if (!isAddress(context.incentiveDataProviderAddress)) {
       throw new Error('contract address is not valid');
     }
+
+    this._context = context;
+
+    this._chainlinkFeedsRegistries = {};
 
     this._contract = UiIncentiveDataProviderFactory.connect(
       context.incentiveDataProviderAddress,
@@ -99,6 +121,29 @@ export class UiIncentiveDataProvider
     }));
   }
 
+  public async getUserReservesIncentivesDataHumanized(
+    user: string,
+    lendingPoolAddressProvider: string,
+  ): Promise<UserReserveIncentiveDataHumanizedResponse[]> {
+    const response = await this.getUserReservesIncentivesData(
+      user,
+      lendingPoolAddressProvider,
+    );
+
+    return response.map(r => ({
+      ...r,
+      aTokenIncentivesUserData: this._formatUserIncentiveData(
+        r.aTokenIncentivesUserData,
+      ),
+      vTokenIncentivesUserData: this._formatUserIncentiveData(
+        r.vTokenIncentivesUserData,
+      ),
+      sTokenIncentivesUserData: this._formatUserIncentiveData(
+        r.sTokenIncentivesUserData,
+      ),
+    }));
+  }
+
   /**
    *  Get the reserve incentive data for the user
    * @param user The user address
@@ -121,13 +166,151 @@ export class UiIncentiveDataProvider
     );
   }
 
+  public async getIncentivesDataWithPrice(
+    lendingPoolAddressProvider: string,
+    chainlinkFeedsRegistry: string,
+    quote: Denominations,
+  ): Promise<ReserveIncentiveWithFeedsResponse[]> {
+    const incentives: ReserveIncentiveDataHumanizedResponse[] = await this.getReservesIncentivesDataHumanized(
+      lendingPoolAddressProvider,
+    );
+
+    if (!this._chainlinkFeedsRegistries[chainlinkFeedsRegistry]) {
+      this._chainlinkFeedsRegistries[
+        chainlinkFeedsRegistry
+      ] = new ChainlinkFeedsRegistry({
+        provider: this._context.provider,
+        chainlinkFeedsRegistry,
+      });
+    }
+
+    const tokensToGetFeed: { [tokenAddress: string]: PriceFeed } = {};
+    for (const incentive of incentives) {
+      if (!tokensToGetFeed[incentive.aIncentiveData.rewardTokenAddress]) {
+        try {
+          tokensToGetFeed[
+            incentive.aIncentiveData.rewardTokenAddress
+            // eslint-disable-next-line no-await-in-loop
+          ] = await this._chainlinkFeedsRegistries[
+            chainlinkFeedsRegistry
+          ].getPriceFeed(incentive.aIncentiveData.rewardTokenAddress, quote);
+        } catch (error) {
+          tokensToGetFeed[
+            incentive.aIncentiveData.rewardTokenAddress
+            // eslint-disable-next-line no-await-in-loop
+          ] = {
+            answer: '0',
+            updatedAt: 0,
+            decimals: 0,
+          };
+        }
+      }
+
+      if (!tokensToGetFeed[incentive.vIncentiveData.rewardTokenAddress]) {
+        try {
+          tokensToGetFeed[
+            incentive.vIncentiveData.rewardTokenAddress
+            // eslint-disable-next-line no-await-in-loop
+          ] = await this._chainlinkFeedsRegistries[
+            chainlinkFeedsRegistry
+          ].getPriceFeed(incentive.vIncentiveData.rewardTokenAddress, quote);
+        } catch (error) {
+          tokensToGetFeed[
+            incentive.aIncentiveData.rewardTokenAddress
+            // eslint-disable-next-line no-await-in-loop
+          ] = {
+            answer: '0',
+            updatedAt: 0,
+            decimals: 0,
+          };
+        }
+      }
+
+      if (!tokensToGetFeed[incentive.sIncentiveData.rewardTokenAddress]) {
+        try {
+          tokensToGetFeed[
+            incentive.sIncentiveData.rewardTokenAddress
+            // eslint-disable-next-line no-await-in-loop
+          ] = await this._chainlinkFeedsRegistries[
+            chainlinkFeedsRegistry
+          ].getPriceFeed(incentive.sIncentiveData.rewardTokenAddress, quote);
+        } catch (error) {
+          tokensToGetFeed[
+            incentive.aIncentiveData.rewardTokenAddress
+            // eslint-disable-next-line no-await-in-loop
+          ] = {
+            answer: '0',
+            updatedAt: 0,
+            decimals: 0,
+          };
+        }
+      }
+    }
+
+    return incentives.map(
+      (incentive: ReserveIncentiveDataHumanizedResponse) => ({
+        underlyingAsset: incentive.underlyingAsset,
+        aIncentiveData: {
+          ...incentive.aIncentiveData,
+          priceFeed:
+            tokensToGetFeed[incentive.aIncentiveData.rewardTokenAddress].answer,
+          priceFeedTimestamp:
+            tokensToGetFeed[incentive.aIncentiveData.rewardTokenAddress]
+              .updatedAt,
+          priceFeedDecimals:
+            tokensToGetFeed[incentive.aIncentiveData.rewardTokenAddress]
+              .decimals,
+        },
+        vIncentiveData: {
+          ...incentive.vIncentiveData,
+          priceFeed:
+            tokensToGetFeed[incentive.vIncentiveData.rewardTokenAddress].answer,
+          priceFeedTimestamp:
+            tokensToGetFeed[incentive.vIncentiveData.rewardTokenAddress]
+              .updatedAt,
+          priceFeedDecimals:
+            tokensToGetFeed[incentive.vIncentiveData.rewardTokenAddress]
+              .decimals,
+        },
+        sIncentiveData: {
+          ...incentive.sIncentiveData,
+          priceFeed:
+            tokensToGetFeed[incentive.sIncentiveData.rewardTokenAddress].answer,
+          priceFeedTimestamp:
+            tokensToGetFeed[incentive.sIncentiveData.rewardTokenAddress]
+              .updatedAt,
+          priceFeedDecimals:
+            tokensToGetFeed[incentive.sIncentiveData.rewardTokenAddress]
+              .decimals,
+        },
+      }),
+    );
+  }
+
   private _formatIncentiveData(data: IncentiveData): IncentiveDataHumanized {
     return {
-      ...data,
+      tokenAddress: data.tokenAddress,
+      precision: data.precision,
+      rewardTokenAddress: data.rewardTokenAddress,
+      incentiveControllerAddress: data.incentiveControllerAddress,
+      rewardTokenDecimals: data.rewardTokenDecimals,
       emissionPerSecond: data.emissionPerSecond.toString(),
       incentivesLastUpdateTimestamp: data.incentivesLastUpdateTimestamp.toNumber(),
       tokenIncentivesIndex: data.tokenIncentivesIndex.toString(),
       emissionEndTimestamp: data.emissionEndTimestamp.toNumber(),
+    };
+  }
+
+  private _formatUserIncentiveData(
+    data: IncentiveUserData,
+  ): IncentiveUserDataHumanized {
+    return {
+      tokenAddress: data.tokenAddress,
+      rewardTokenAddress: data.rewardTokenAddress,
+      incentiveControllerAddress: data.incentiveControllerAddress,
+      rewardTokenDecimals: data.rewardTokenDecimals,
+      tokenincentivesUserIndex: data.tokenincentivesUserIndex.toString(),
+      userUnclaimedRewards: data.userUnclaimedRewards.toString(),
     };
   }
 }
