633880764555d5589b601e7f5983a8be97b764bf
sendra <polsendra@gmail.com>
2021-10-11 14:35:38 +0200

    fix: fixed types

--- packages/contract-helpers/src/ui-pool-data-provider/types/UiPoolDataProviderTypes.ts
+++ packages/contract-helpers/src/ui-pool-data-provider/types/UiPoolDataProviderTypes.ts
@@ -1,80 +1,83 @@
 import { BigNumber } from 'ethers';
 
-export interface ReserveData {
-  underlyingAsset: string;
-  name: string;
-  symbol: string;
-  decimals: BigNumber;
-  baseLTVasCollateral: BigNumber;
-  reserveLiquidationThreshold: BigNumber;
-  reserveLiquidationBonus: BigNumber;
-  reserveFactor: BigNumber;
-  usageAsCollateralEnabled: boolean;
-  borrowingEnabled: boolean;
-  stableBorrowRateEnabled: boolean;
-  isActive: boolean;
-  isFrozen: boolean;
-  liquidityIndex: BigNumber;
-  variableBorrowIndex: BigNumber;
-  liquidityRate: BigNumber;
-  variableBorrowRate: BigNumber;
-  stableBorrowRate: BigNumber;
-  lastUpdateTimestamp: number;
-  aTokenAddress: string;
-  stableDebtTokenAddress: string;
-  variableDebtTokenAddress: string;
-  interestRateStrategyAddress: string;
-  availableLiquidity: BigNumber;
-  totalPrincipalStableDebt: BigNumber;
-  averageStableRate: BigNumber;
-  stableDebtLastUpdateTimestamp: BigNumber;
-  totalScaledVariableDebt: BigNumber;
-  priceForAsset: BigNumber;
-  variableRateSlope1: BigNumber;
-  variableRateSlope2: BigNumber;
-  stableRateSlope1: BigNumber;
-  stableRateSlope2: BigNumber;
-  0: string;
-  1: string;
-  2: string;
-  3: BigNumber;
-  4: BigNumber;
-  5: BigNumber;
-  6: BigNumber;
-  7: BigNumber;
-  8: boolean;
-  9: boolean;
-  10: boolean;
-  11: boolean;
-  12: boolean;
-  13: BigNumber;
-  14: BigNumber;
-  15: BigNumber;
-  16: BigNumber;
-  17: BigNumber;
-  18: number;
-  19: string;
-  20: string;
-  21: string;
-  22: string;
-  23: BigNumber;
-  24: BigNumber;
-  25: BigNumber;
-  26: BigNumber;
-  27: BigNumber;
-  28: BigNumber;
-  29: BigNumber;
-  30: BigNumber;
-  31: BigNumber;
-  32: BigNumber;
+export interface ReservesData {
+  0: Array<{
+    underlyingAsset: string;
+    name: string;
+    symbol: string;
+    decimals: BigNumber;
+    baseLTVasCollateral: BigNumber;
+    reserveLiquidationThreshold: BigNumber;
+    reserveLiquidationBonus: BigNumber;
+    reserveFactor: BigNumber;
+    usageAsCollateralEnabled: boolean;
+    borrowingEnabled: boolean;
+    stableBorrowRateEnabled: boolean;
+    isActive: boolean;
+    isFrozen: boolean;
+    liquidityIndex: BigNumber;
+    variableBorrowIndex: BigNumber;
+    liquidityRate: BigNumber;
+    variableBorrowRate: BigNumber;
+    stableBorrowRate: BigNumber;
+    lastUpdateTimestamp: number;
+    aTokenAddress: string;
+    stableDebtTokenAddress: string;
+    variableDebtTokenAddress: string;
+    interestRateStrategyAddress: string;
+    availableLiquidity: BigNumber;
+    totalPrincipalStableDebt: BigNumber;
+    averageStableRate: BigNumber;
+    stableDebtLastUpdateTimestamp: BigNumber;
+    totalScaledVariableDebt: BigNumber;
+    priceInMarketReferenceCurrency: BigNumber;
+    variableRateSlope1: BigNumber;
+    variableRateSlope2: BigNumber;
+    stableRateSlope1: BigNumber;
+    stableRateSlope2: BigNumber;
+    0: string;
+    1: string;
+    2: string;
+    3: BigNumber;
+    4: BigNumber;
+    5: BigNumber;
+    6: BigNumber;
+    7: BigNumber;
+    8: boolean;
+    9: boolean;
+    10: boolean;
+    11: boolean;
+    12: boolean;
+    13: BigNumber;
+    14: BigNumber;
+    15: BigNumber;
+    16: BigNumber;
+    17: BigNumber;
+    18: number;
+    19: string;
+    20: string;
+    21: string;
+    22: string;
+    23: BigNumber;
+    24: BigNumber;
+    25: BigNumber;
+    26: BigNumber;
+    27: BigNumber;
+    28: BigNumber;
+    29: BigNumber;
+    30: BigNumber;
+    31: BigNumber;
+    32: BigNumber;
+  }>;
+  1: {
+    baseCurrencyDecimals: BigNumber;
+    baseCurrencyPriceInUsd: BigNumber;
+    0: BigNumber;
+    1: BigNumber;
+  };
 }
 
-export interface ReserveDataResponse {
-  0: ReserveData[];
-  1: BigNumber;
-}
-
-export interface UserReserveDataResponse {
+export interface UserReserveData {
   underlyingAsset: string;
   scaledATokenBalance: BigNumber;
   usageAsCollateralEnabledOnUser: boolean;
@@ -89,11 +92,59 @@ export interface UserReserveDataResponse {
   4: BigNumber;
   5: BigNumber;
   6: BigNumber;
-  7: BigNumber;
 }
 
-export interface AllReserveDataResponse {
-  0: ReserveData[];
-  1: UserReserveDataResponse[];
-  2: BigNumber;
+export interface PoolBaseCurrencyHumanized {
+  baseCurrencyDecimals: number;
+  baseCurrencyPriceInUsd: string;
+}
+export interface ReserveDataHumanized {
+  underlyingAsset: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  baseLTVasCollateral: string;
+  reserveLiquidationThreshold: string;
+  reserveLiquidationBonus: string;
+  reserveFactor: string;
+  usageAsCollateralEnabled: boolean;
+  borrowingEnabled: boolean;
+  stableBorrowRateEnabled: boolean;
+  isActive: boolean;
+  isFrozen: boolean;
+  liquidityIndex: string;
+  variableBorrowIndex: string;
+  liquidityRate: string;
+  variableBorrowRate: string;
+  stableBorrowRate: string;
+  lastUpdateTimestamp: number;
+  aTokenAddress: string;
+  stableDebtTokenAddress: string;
+  variableDebtTokenAddress: string;
+  interestRateStrategyAddress: string;
+  availableLiquidity: string;
+  totalPrincipalStableDebt: string;
+  averageStableRate: string;
+  stableDebtLastUpdateTimestamp: number;
+  totalScaledVariableDebt: string;
+  priceInMarketReferenceCurrency: string;
+  variableRateSlope1: string;
+  variableRateSlope2: string;
+  stableRateSlope1: string;
+  stableRateSlope2: string;
+}
+
+export interface ReservesDataHumanized {
+  reservesData: ReserveDataHumanized[];
+  baseCurrencyData: PoolBaseCurrencyHumanized;
+}
+
+export interface UserReserveDataHumanized {
+  underlyingAsset: string;
+  scaledATokenBalance: string;
+  usageAsCollateralEnabledOnUser: boolean;
+  stableBorrowRate: string;
+  scaledVariableDebt: string;
+  principalStableDebt: string;
+  stableBorrowLastUpdateTimestamp: number;
 }
