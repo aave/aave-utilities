bcd4226c095f00801098ea2712ed60aeb2b9d159
sakulstra <lukasstrassel@googlemail.com>
2021-10-14 14:34:08 +0200

    fix: remove import

--- packages/math-utils/src/formatters/user/index.test.ts
+++ packages/math-utils/src/formatters/user/index.test.ts
@@ -1,10 +1,6 @@
 import BigNumber from 'bignumber.js';
 import { formatUserSummaryRequest } from './user.mocks';
-import {
-  ComputedUserReserve,
-  formatUserSummary,
-  FormatUserSummaryRequest,
-} from './index';
+import { formatUserSummary, FormatUserSummaryRequest } from './index';
 
 describe('formatUserSummary', () => {
   const request: FormatUserSummaryRequest = formatUserSummaryRequest;
