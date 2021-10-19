e342d9f171d0286d2cc0ed57cb9044146aca55eb
Andrew Schmidt <andrew@aave.com>
2021-10-18 19:50:11 -0500

    feat: add function for computing all reserve incentives

---
+++ packages/math-utils/src/formatters/incentive/calculate-all-reserve-incentives.test.ts
@@ -0,0 +1,119 @@
+import { calculateAllReserveIncentives } from './calculate-all-reserve-incentives';
+import {
+  reserveIncentives,
+  allIncentivesReservesWithRewardReserve,
+  maticReserveIncentives,
+  avaxReserveIncentives,
+  allIncentivesReserves,
+} from './incentive.mocks';
+
+describe('calculateAllReserveIncentives', () => {
+  it('calculates correct incentives data for each reserve asset', () => {
+    const result = calculateAllReserveIncentives({
+      reserveIncentives,
+      reserves: allIncentivesReservesWithRewardReserve,
+    });
+    expect(
+      result['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].aIncentives
+        .incentiveAPY,
+    ).toBe('0.00246430102947281231');
+    expect(
+      result['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].vIncentives
+        .incentiveAPY,
+    ).toBe('0.00315794307743075208');
+    expect(
+      result['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].sIncentives
+        .incentiveAPY,
+    ).toBe('0');
+
+    expect(
+      result['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'].aIncentives
+        .incentiveAPY,
+    ).toBe('0.0420759866182279689');
+    expect(
+      result['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'].vIncentives
+        .incentiveAPY,
+    ).toBe('0.04740458788219602162');
+    expect(
+      result['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'].sIncentives
+        .incentiveAPY,
+    ).toBe('0');
+
+    expect(
+      result['0x8798249c2e607446efb7ad49ec89dd1865ff4272'].aIncentives
+        .incentiveAPY,
+    ).toBe('0.00957358991380907344');
+    expect(
+      result['0x8798249c2e607446efb7ad49ec89dd1865ff4272'].vIncentives
+        .incentiveAPY,
+    ).toBe('0');
+    expect(
+      result['0x8798249c2e607446efb7ad49ec89dd1865ff4272'].sIncentives
+        .incentiveAPY,
+    ).toBe('0');
+  });
+
+  it('calculates correct incentives with priceFeed from incentives data', () => {
+    const result = calculateAllReserveIncentives({
+      reserveIncentives,
+      reserves: allIncentivesReserves,
+    });
+    // Results will be slightly different from above due to 8 vs 18 decimal precision of priceFeed vs priceInMarketReferenceCurrency
+    expect(
+      result['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].aIncentives
+        .incentiveAPY,
+    ).toBe('0.00246430081696612272');
+    expect(
+      result['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].vIncentives
+        .incentiveAPY,
+    ).toBe('0.0031579428051084906');
+    expect(
+      result['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].sIncentives
+        .incentiveAPY,
+    ).toBe('0');
+    expect(
+      result['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'].aIncentives
+        .incentiveAPY,
+    ).toBe('0.04207598298984469896');
+    expect(
+      result['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'].vIncentives
+        .incentiveAPY,
+    ).toBe('0.04740458379430581596');
+    expect(
+      result['0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'].sIncentives
+        .incentiveAPY,
+    ).toBe('0');
+
+    expect(
+      result['0x8798249c2e607446efb7ad49ec89dd1865ff4272'].aIncentives
+        .incentiveAPY,
+    ).toBe('0.00957358908823952015');
+    expect(
+      result['0x8798249c2e607446efb7ad49ec89dd1865ff4272'].vIncentives
+        .incentiveAPY,
+    ).toBe('0');
+    expect(
+      result['0x8798249c2e607446efb7ad49ec89dd1865ff4272'].sIncentives
+        .incentiveAPY,
+    ).toBe('0');
+  });
+
+  it('calculates correct underlyingAsset and rewardTokenAddress for base assets', () => {
+    const maticResult = calculateAllReserveIncentives({
+      reserveIncentives,
+      reserves: maticReserveIncentives,
+    });
+    const avaxResult = calculateAllReserveIncentives({
+      reserveIncentives,
+      reserves: avaxReserveIncentives,
+    });
+    expect(
+      maticResult['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].aIncentives
+        .rewardTokenAddress,
+    ).toBe('0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270');
+    expect(
+      avaxResult['0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'].aIncentives
+        .rewardTokenAddress,
+    ).toBe('0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7');
+  });
+});
---
+++ packages/math-utils/src/formatters/incentive/calculate-all-reserve-incentives.ts
@@ -0,0 +1,151 @@
+import BigNumber from 'bignumber.js';
+import { ReserveIncentiveWithFeedsResponse } from 'contract-helpers';
+import {
+  calculateReserveIncentives,
+  CalculateReserveIncentivesResponse,
+} from './calculate-reserve-incentives';
+
+export interface ReserveData {
+  underlyingAsset: string;
+  symbol: string;
+  totalLiquidity: string;
+  liquidityIndex: string;
+  totalScaledVariableDebt: string;
+  totalPrincipalStableDebt: string;
+  priceInMarketReferenceCurrency: string;
+  marketReferenceCurrencyDecimals: number;
+  decimals: number;
+}
+
+// Indexed by reserve underlyingAsset address
+export type ReserveIncentiveDict = Record<string, ReserveIncentives>;
+
+interface ReserveIncentives {
+  aIncentives: ReserveIncentive;
+  vIncentives: ReserveIncentive;
+  sIncentives: ReserveIncentive;
+}
+
+interface ReserveIncentive {
+  incentiveAPY: string;
+  rewardTokenAddress: string;
+}
+
+export interface CalculateAllReserveIncentivesRequest {
+  reserveIncentives: ReserveIncentiveWithFeedsResponse[];
+  reserves: ReserveData[];
+}
+
+// Calculate incentive token price from reserves data or priceFeed from UiIncentiveDataProvider
+// eslint-disable-next-line max-params
+function calculateRewardTokenPrice(
+  reserves: ReserveData[],
+  address: string,
+  priceFeed: string,
+  priceFeedDecimals: number,
+  marketReferenceCurrencyDecimals: number,
+): string {
+  address = address.toLowerCase();
+  // For stkAave incentives, use Aave reserve data
+  if (address === '0x4da27a545c0c5b758a6ba100e3a049001de870f5') {
+    address = '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9';
+  }
+
+  // For WMATIC/WAVAX incentives, use MATIC/AVAX reserve data
+  if (
+    address === '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270' ||
+    address === '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7'
+  ) {
+    address = '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee';
+  }
+
+  const rewardReserve = reserves.find(
+    reserve => reserve.underlyingAsset.toLowerCase() === address,
+  );
+  if (rewardReserve) {
+    return rewardReserve.priceInMarketReferenceCurrency;
+  }
+
+  // Convert priceFeed to have same number of decimals as marketReferenceCurrency
+  return new BigNumber(priceFeed)
+    .shiftedBy(marketReferenceCurrencyDecimals - priceFeedDecimals)
+    .toString();
+}
+
+export function calculateAllReserveIncentives({
+  reserveIncentives,
+  reserves,
+}: CalculateAllReserveIncentivesRequest): ReserveIncentiveDict {
+  const reserveDict: ReserveIncentiveDict = {};
+  // calculate incentive per reserve token
+  reserves.forEach(reserve => {
+    // Account for underlyingReserveAddress of network base assets not matching wrapped incentives
+    let reserveUnderlyingAddress = reserve.underlyingAsset.toLowerCase();
+    let isBaseAsset = false;
+    if (
+      reserveUnderlyingAddress === '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+    ) {
+      isBaseAsset = true;
+      // Disable test coverage for else block: added tests for all conditions and can verify execution, jest doesn't detect coverage
+      /* istanbul ignore else */
+      if (reserve.symbol === 'MATIC') {
+        reserveUnderlyingAddress = '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270';
+      } else if (reserve.symbol === 'ETH') {
+        reserveUnderlyingAddress = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
+      } else if (reserve.symbol === 'AVAX') {
+        reserveUnderlyingAddress = '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7';
+      }
+    }
+
+    // Find the corresponding incentive data for each reserve
+    const incentiveData = reserveIncentives.find(
+      incentive =>
+        incentive.underlyingAsset.toLowerCase() === reserveUnderlyingAddress,
+    );
+    if (incentiveData) {
+      const calculatedReserveIncentives: CalculateReserveIncentivesResponse = calculateReserveIncentives(
+        {
+          reserveIncentiveData: incentiveData,
+          totalLiquidity: reserve.totalLiquidity,
+          liquidityIndex: reserve.liquidityIndex,
+          totalScaledVariableDebt: reserve.totalScaledVariableDebt,
+          totalPrincipalStableDebt: reserve.totalPrincipalStableDebt,
+          priceInMarketReferenceCurrency:
+            reserve.priceInMarketReferenceCurrency,
+          decimals: reserve.decimals,
+          aRewardTokenPriceInMarketReferenceCurrency: calculateRewardTokenPrice(
+            reserves,
+            incentiveData.aIncentiveData.rewardTokenAddress.toLowerCase(),
+            incentiveData.aIncentiveData.priceFeed,
+            incentiveData.aIncentiveData.priceFeedDecimals,
+            reserve.marketReferenceCurrencyDecimals,
+          ),
+          vRewardTokenPriceInMarketReferenceCurrency: calculateRewardTokenPrice(
+            reserves,
+            incentiveData.vIncentiveData.rewardTokenAddress.toLowerCase(),
+            incentiveData.vIncentiveData.priceFeed,
+            incentiveData.vIncentiveData.priceFeedDecimals,
+            reserve.marketReferenceCurrencyDecimals,
+          ),
+          sRewardTokenPriceInMarketReferenceCurrency: calculateRewardTokenPrice(
+            reserves,
+            incentiveData.sIncentiveData.rewardTokenAddress.toLowerCase(),
+            incentiveData.sIncentiveData.priceFeed,
+            incentiveData.sIncentiveData.priceFeedDecimals,
+            reserve.marketReferenceCurrencyDecimals,
+          ),
+        },
+      );
+      calculatedReserveIncentives.underlyingAsset = isBaseAsset
+        ? '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee'
+        : calculatedReserveIncentives.underlyingAsset; // ETH, MATIC, and AVAX all use reserve address of '0xee...'
+      reserveDict[calculatedReserveIncentives.underlyingAsset] = {
+        aIncentives: calculatedReserveIncentives.aIncentivesData,
+        vIncentives: calculatedReserveIncentives.vIncentivesData,
+        sIncentives: calculatedReserveIncentives.sIncentivesData,
+      };
+    }
+  });
+
+  return reserveDict;
+}
