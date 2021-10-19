99b8499734235442af3c62d906a2bb09c82f899d
Andrew Schmidt <andrew@aave.com>
2021-10-18 19:51:23 -0500

    feat: update incentive module exports

--- packages/math-utils/src/formatters/incentive/index.ts
+++ packages/math-utils/src/formatters/incentive/index.ts
@@ -1,2 +1,2 @@
-export * from './calculate-total-user-incentives';
-export * from './calculate-reserve-incentives';
+export * from './calculate-all-user-incentives';
+export * from './calculate-all-reserve-incentives';
