bd7adc7484ec3ac5c7efc563130c78d02633b7a8
Andrew Schmidt <andrew@aave.com>
2021-10-18 19:51:02 -0500

    feat: update mock incentives for testing

--- packages/math-utils/src/formatters/incentive/incentive.mocks.ts
+++ packages/math-utils/src/formatters/incentive/incentive.mocks.ts
@@ -8,10 +8,13 @@ export const aETHReserveIncentiveData = {
     tokenIncentivesIndex: '24733519699535219',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000a',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
   vIncentiveData: {
     emissionPerSecond: '104166666666666',
@@ -19,10 +22,13 @@ export const aETHReserveIncentiveData = {
     tokenIncentivesIndex: '26465727412280876',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000v',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
   sIncentiveData: {
     emissionPerSecond: '0',
@@ -30,10 +36,13 @@ export const aETHReserveIncentiveData = {
     tokenIncentivesIndex: '0',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000s',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
 };
 
@@ -45,10 +54,13 @@ export const aUSDCReserveIncentiveData = {
     tokenIncentivesIndex: '16055272805126608426463068',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000a',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
   vIncentiveData: {
     emissionPerSecond: '4629629629629629',
@@ -56,10 +68,13 @@ export const aUSDCReserveIncentiveData = {
     tokenIncentivesIndex: '21239260963515875537848724',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000v',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
   sIncentiveData: {
     emissionPerSecond: '0',
@@ -67,10 +82,13 @@ export const aUSDCReserveIncentiveData = {
     tokenIncentivesIndex: '0',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000s',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
 };
 
@@ -82,10 +100,13 @@ export const aXSUSHIReserveIncentiveData = {
     tokenIncentivesIndex: '23156815865521',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000a',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
   vIncentiveData: {
     emissionPerSecond: '0',
@@ -93,10 +114,13 @@ export const aXSUSHIReserveIncentiveData = {
     tokenIncentivesIndex: '0',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000v',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
   sIncentiveData: {
     emissionPerSecond: '0',
@@ -104,11 +128,56 @@ export const aXSUSHIReserveIncentiveData = {
     tokenIncentivesIndex: '0',
     emissionEndTimestamp: 1637573428,
     tokenAddress: '0x000000000000000000000000000000000000000s',
-    rewardTokenAddress: '0x0000000000000000000000000000000000000000',
+    rewardTokenAddress: '0x4da27a545c0c5b758a6ba100e3a049001de870f5',
     incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
     rewardTokenDecimals: 18,
     precision: 18,
+    priceFeed: '7853038',
+    priceFeedTimestamp: 1000000000,
+    priceFeedDecimals: 8,
   },
+};
+
+const maticIncentives = {
+  emissionPerSecond: '173611111111111',
+  incentivesLastUpdateTimestamp: 1631586256,
+  tokenIncentivesIndex: '23156815865521',
+  emissionEndTimestamp: 1637573428,
+  tokenAddress: '0x000000000000000000000000000000000000000a',
+  rewardTokenAddress: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+  incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
+  rewardTokenDecimals: 18,
+  precision: 18,
+  priceFeed: '7853038',
+  priceFeedTimestamp: 1000000000,
+  priceFeedDecimals: 8,
+};
+export const aMATICReserveIncentiveData = {
+  underlyingAsset: '0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270',
+  aIncentiveData: maticIncentives,
+  vIncentiveData: maticIncentives,
+  sIncentiveData: maticIncentives,
+};
+
+const avaxIncentives = {
+  emissionPerSecond: '173611111111111',
+  incentivesLastUpdateTimestamp: 1631586256,
+  tokenIncentivesIndex: '23156815865521',
+  emissionEndTimestamp: 1637573428,
+  tokenAddress: '0x000000000000000000000000000000000000000a',
+  rewardTokenAddress: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
+  incentiveControllerAddress: '0x0000000000000000000000000000000000000000',
+  rewardTokenDecimals: 18,
+  precision: 18,
+  priceFeed: '7853038',
+  priceFeedTimestamp: 1000000000,
+  priceFeedDecimals: 8,
+};
+export const aAVAXReserveIncentiveData = {
+  underlyingAsset: '0xb31f66aa3c1e785363f0875a1b74e27b85fd66c7',
+  aIncentiveData: avaxIncentives,
+  vIncentiveData: avaxIncentives,
+  sIncentiveData: avaxIncentives,
 };
 
 // User Incentives
@@ -225,11 +294,13 @@ export const aUSDCUserIncentiveDataMultiController = {
   },
 };
 
-// Total incentives inputs
+// Total user incentives inputs
 export const reserveIncentives = [
   aETHReserveIncentiveData,
   aUSDCReserveIncentiveData,
   aXSUSHIReserveIncentiveData,
+  aMATICReserveIncentiveData,
+  aAVAXReserveIncentiveData,
 ];
 export const reserveIncentivesMissingUSDC = [
   aETHReserveIncentiveData,
@@ -272,3 +343,87 @@ export const aXSUSHIReserve = {
   principalStableDebt: '0',
 };
 export const userReserves = [aETHReserve, aUSDCReserve, aXSUSHIReserve];
+
+// Total reserve incentives inputs
+
+export const allIncentivesReserves = [
+  {
+    underlyingAsset: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+    symbol: 'ETH',
+    totalLiquidity: '2003772886310189627431436',
+    liquidityIndex: '1007431539067282236768346040',
+    totalScaledVariableDebt: '81689984341288838884434',
+    totalPrincipalStableDebt: '681322787738991248642',
+    priceInMarketReferenceCurrency: '1000000000000000000',
+    marketReferenceCurrencyDecimals: 18,
+    decimals: 18,
+  },
+  {
+    underlyingAsset: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
+    symbol: 'USDC',
+    totalLiquidity: '1037303607422151',
+    liquidityIndex: '1016944509782990800790457707',
+    totalScaledVariableDebt: '905362631532314',
+    totalPrincipalStableDebt: '0',
+    priceInMarketReferenceCurrency: '267145370000000',
+    marketReferenceCurrencyDecimals: 18,
+    decimals: 6,
+  },
+  {
+    underlyingAsset: '0x8798249c2e607446efb7ad49ec89dd1865ff4272',
+    symbol: 'XSUSHI',
+    totalLiquidity: '13051166036525999213246575',
+    liquidityIndex: '1000133668402084314111784727',
+    totalScaledVariableDebt: '404110132365797056457861',
+    totalPrincipalStableDebt: '0',
+    priceInMarketReferenceCurrency: '3441563382242556',
+    marketReferenceCurrencyDecimals: 18,
+    decimals: 18,
+  },
+];
+
+// Used as price feed for stkAave incentives
+const aaveReserve = {
+  underlyingAsset: '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9',
+  symbol: 'AAVE',
+  totalLiquidity: '0',
+  liquidityIndex: '0',
+  totalScaledVariableDebt: '0',
+  totalPrincipalStableDebt: '0',
+  priceInMarketReferenceCurrency: '78530386771994300',
+  marketReferenceCurrencyDecimals: 18,
+  decimals: 18,
+};
+
+const maticReserve = {
+  underlyingAsset: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  symbol: 'MATIC',
+  totalLiquidity: '0',
+  liquidityIndex: '0',
+  totalScaledVariableDebt: '0',
+  totalPrincipalStableDebt: '0',
+  priceInMarketReferenceCurrency: '399997866678044',
+  decimals: 18,
+  marketReferenceCurrencyDecimals: 18,
+};
+
+const avaxReserve = {
+  underlyingAsset: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
+  symbol: 'AVAX',
+  totalLiquidity: '0',
+  liquidityIndex: '0',
+  totalScaledVariableDebt: '0',
+  totalPrincipalStableDebt: '0',
+  priceInMarketReferenceCurrency: '5522081986',
+  decimals: 18,
+  marketReferenceCurrencyDecimals: 8,
+};
+
+export const allIncentivesReservesWithRewardReserve = [
+  ...allIncentivesReserves,
+  aaveReserve,
+];
+
+// These must be separate test case because only one reserve can use base asset address (0xeee...) at a time
+export const maticReserveIncentives = [maticReserve];
+export const avaxReserveIncentives = [avaxReserve];
