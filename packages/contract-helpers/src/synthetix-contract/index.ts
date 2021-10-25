8f87c14a2e93def7622fe3e94816b8657c68bcc1
Pol Sendra <sendra@users.noreply.github.com>
2021-10-25 13:06:20 +0200

    feat: ported synthetix service (#123)
    * feat: ported synthetix service
    
    * feat: added synthetix contract test. coverage 100%

---
+++ packages/contract-helpers/src/synthetix-contract/index.ts
@@ -0,0 +1,61 @@
+import { BigNumber, providers } from 'ethers';
+import BaseService from '../commons/BaseService';
+import { ChainId } from '../commons/types';
+import { SynthetixValidator } from '../commons/validators/methodValidators';
+import {
+  isEthAddress,
+  isPositiveAmount,
+} from '../commons/validators/paramValidators';
+import { ISynthetix } from './typechain/ISynthetix';
+import { ISynthetix__factory } from './typechain/ISynthetix__factory';
+
+export const synthetixProxyByChainId: Record<number, string> = {
+  [ChainId.mainnet]: '0xc011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f',
+};
+
+export type SynthetixValidationType = {
+  user: string;
+  reserve: string;
+  amount: string; // wei
+};
+
+export interface SynthetixInterface {
+  synthetixValidation: (args: SynthetixValidationType) => Promise<boolean>;
+}
+
+export class SynthetixService
+  extends BaseService<ISynthetix>
+  implements SynthetixInterface
+{
+  constructor(provider: providers.Provider) {
+    super(provider, ISynthetix__factory);
+  }
+
+  @SynthetixValidator
+  public async synthetixValidation(
+    @isEthAddress('user')
+    @isEthAddress('reserve')
+    @isPositiveAmount('amount')
+    {
+      user,
+      reserve,
+      amount, // wei
+    }: SynthetixValidationType,
+  ): Promise<boolean> {
+    const { chainId } = await this.provider.getNetwork();
+    if (
+      synthetixProxyByChainId[chainId] &&
+      reserve.toLowerCase() === synthetixProxyByChainId[chainId].toLowerCase()
+    ) {
+      const synthContract = this.getContractInstance(
+        synthetixProxyByChainId[chainId],
+      );
+
+      const transferableAmount: BigNumber =
+        await synthContract.transferableSynthetix(user);
+      return BigNumber.from(amount).lte(transferableAmount);
+    }
+
+    return true;
+  }
+}
