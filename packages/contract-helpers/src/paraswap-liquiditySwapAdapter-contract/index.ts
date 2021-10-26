db86ca2068d70d9889bce43a803e50d49214caac
sendra <polsendra@gmail.com>
2021-10-26 13:08:35 +0200

    feat: coverage to 100%

--- packages/contract-helpers/src/paraswap-liquiditySwapAdapter-contract/index.ts
+++ packages/contract-helpers/src/paraswap-liquiditySwapAdapter-contract/index.ts
@@ -16,18 +16,28 @@ import {
 import { IParaSwapLiquiditySwapAdapter } from './typechain/IParaSwapLiquiditySwapAdapter';
 import { IParaSwapLiquiditySwapAdapter__factory } from './typechain/IParaSwapLiquiditySwapAdapter__factory';
 
-export function augustusFromAmountOffsetFromCalldata(calldata: string) {
+export function augustusFromAmountOffsetFromCalldata(calldata: string): number {
   switch (calldata.slice(0, 10)) {
     case '0xda8567c8': // Augustus V3 multiSwap
-      return 4 + 32 + 2 * 32;
+      return 100; // 4 + 3 * 32
     case '0x58b9d179': // Augustus V4 swapOnUniswap
-      return 4;
+      return 4; // 4 + 0 * 32
     case '0x0863b7ac': // Augustus V4 swapOnUniswapFork
-      return 4 + 2 * 32;
+      return 68; // 4 + 2 * 32
     case '0x8f00eccb': // Augustus V4 multiSwap
-      return 4 + 32 + 32;
+      return 68; // 4 + 2 * 32
     case '0xec1d21dd': // Augustus V4 megaSwap
-      return 4 + 32 + 32;
+      return 68; // 4 + 2 * 32
+    case '0x54840d1a': // Augustus V5 swapOnUniswap
+      return 4; // 4 + 0 * 32
+    case '0xf5661034': // Augustus V5 swapOnUniswapFork
+      return 68; // 4 + 2 * 32
+    case '0x64466805': // Augustus V5 swapOnZeroXv4
+      return 68; // 4 + 2 * 32
+    case '0xa94e78ef': // Augustus V5 multiSwap
+      return 68; // 4 + 2 * 32
+    case '0x46c67b6d': // Augustus V5 megaSwap
+      return 68; // 4 + 2 * 32
     default:
       throw new Error('Unrecognized function selector for Augustus');
   }
@@ -37,8 +47,8 @@ export type SwapAndDepositMethodType = {
   user: tEthereumAddress;
   assetToSwapFrom: tEthereumAddress;
   assetToSwapTo: tEthereumAddress;
-  amountToSwap: string;
-  minAmountToReceive: string;
+  amountToSwap: string; // wei
+  minAmountToReceive: string; // wei
   permitParams: PermitSignature;
   swapCallData: BytesLike;
   augustus: tEthereumAddress;
@@ -52,7 +62,7 @@ export interface LiquiditySwapAdapterInterface {
   ) => EthereumTransactionTypeExtended;
 }
 
-export default class LiquiditySwapAdapterService
+export class LiquiditySwapAdapterService
   extends BaseService<IParaSwapLiquiditySwapAdapter>
   implements LiquiditySwapAdapterInterface
 {
@@ -60,7 +70,7 @@ export default class LiquiditySwapAdapterService
 
   constructor(
     provider: providers.Provider,
-    swapCollateralAdapterAddress: string | undefined,
+    swapCollateralAdapterAddress?: string,
   ) {
     super(provider, IParaSwapLiquiditySwapAdapter__factory);
 
