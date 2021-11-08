09584fede051cfc2be0c05d496d31018431a69f2
Pol Sendra <sendra@users.noreply.github.com>
2021-11-08 12:58:55 +0100

    feat: added new fields to v3 uipooldataprovider (#154)
    * feat: added new fields to new uipooldataprovider
    
    * feat: updated types and tests
    
    * fix: ignore factory for 100 cover
    
    * feat: renamed to v3

--- packages/contract-helpers/CHANGELOG.md
+++ packages/contract-helpers/CHANGELOG.md
@@ -1,29 +1,23 @@
 # Change Log
 
-All notable changes to this project will be documented in this file.
-See [Conventional Commits](https://conventionalcommits.org) for commit guidelines.
+All notable changes to this project will be documented in this file. See
+[Conventional Commits](https://conventionalcommits.org) for commit guidelines.
 
 # 0.23.0 (2021-11-05)
 
-
 ### Features
 
-* delegation tx builder ([#156](https://github.com/@aave/aave-utilities/issues/156)) ([6abae6d](https://github.com/@aave/aave-utilities/commit/6abae6da9204d39cbf30e447a2ba76a6fdc43242))
-
-
-
-
+- delegation tx builder
+  ([#156](https://github.com/@aave/aave-utilities/issues/156))
+  ([6abae6d](https://github.com/@aave/aave-utilities/commit/6abae6da9204d39cbf30e447a2ba76a6fdc43242))
 
 # 0.22.0 (2021-11-04)
 
-
 ### Features
 
-* governance voting contract ([#151](https://github.com/@aave/aave-utilities/issues/151)) ([ff6726f](https://github.com/@aave/aave-utilities/commit/ff6726fc1333b5fbb2794a472e82f6660f89e05d))
-
-
-
-
+- governance voting contract
+  ([#151](https://github.com/@aave/aave-utilities/issues/151))
+  ([ff6726f](https://github.com/@aave/aave-utilities/commit/ff6726fc1333b5fbb2794a472e82f6660f89e05d))
 
 # Change Log
 
---
+++ packages/contract-helpers/src/v3-UiPoolDataProvider-contract/_mocks.ts
@@ -0,0 +1,283 @@
+import { BigNumber } from 'ethers';
+import { ReservesData, UserReserveData } from './types';
+
+export const reservesMock: ReservesData = {
+  0: [
+    {
+      underlyingAsset: '0x3E0437898a5667a4769B1Ca5A34aAB1ae7E81377',
+      name: '',
+      symbol: 'AMPL',
+      decimals: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      baseLTVasCollateral: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationThreshold: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationBonus: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveFactor: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      usageAsCollateralEnabled: false,
+      borrowingEnabled: true,
+      stableBorrowRateEnabled: false,
+      isActive: true,
+      isFrozen: false,
+      isPaused: false,
+      liquidityIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      liquidityRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      lastUpdateTimestamp: 1631772892,
+      aTokenAddress: '0xb8a16bbab34FA7A5C09Ec7679EAfb8fEC06897bc',
+      stableDebtTokenAddress: '0x9157d57DC97A7AFFC7b0a78E78fe25e1401B1dCc',
+      variableDebtTokenAddress: '0xb7b7AF565495670713C92B8848fC8A650a968F81',
+      interestRateStrategyAddress: '0x796ec26fc7df8D81BCB5BABF74ccdE0E2B122164',
+      availableLiquidity: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      totalPrincipalStableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      averageStableRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableDebtLastUpdateTimestamp: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      totalScaledVariableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      priceInMarketReferenceCurrency: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableRateSlope1: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableRateSlope2: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableRateSlope1: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableRateSlope2: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      // new
+      debtCeiling: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      eModeCategoryId: 1,
+      borrowCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      supplyCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      eModeLtv: 1,
+      eModeLiquidationThreshold: 1,
+      eModeLiquidationBonus: 1,
+      eModePriceSource: '0x3E0437898a5667a4769B1Ca5A34aAB1ae7E81377',
+      eModeLabel: 'test label',
+    },
+    {
+      underlyingAsset: '0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11',
+      name: '',
+      symbol: 'UNI-V2',
+      decimals: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      baseLTVasCollateral: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationThreshold: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationBonus: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveFactor: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      usageAsCollateralEnabled: false,
+      borrowingEnabled: true,
+      stableBorrowRateEnabled: false,
+      isActive: true,
+      isFrozen: false,
+      isPaused: false,
+      liquidityIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      liquidityRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      lastUpdateTimestamp: 1631772892,
+      aTokenAddress: '0xb8a16bbab34FA7A5C09Ec7679EAfb8fEC06897bc',
+      stableDebtTokenAddress: '0x9157d57DC97A7AFFC7b0a78E78fe25e1401B1dCc',
+      variableDebtTokenAddress: '0xb7b7AF565495670713C92B8848fC8A650a968F81',
+      interestRateStrategyAddress: '0x796ec26fc7df8D81BCB5BABF74ccdE0E2B122164',
+      availableLiquidity: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      totalPrincipalStableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      averageStableRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableDebtLastUpdateTimestamp: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      totalScaledVariableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      priceInMarketReferenceCurrency: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableRateSlope1: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableRateSlope2: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableRateSlope1: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableRateSlope2: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      // new
+      debtCeiling: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      eModeCategoryId: 1,
+      borrowCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      supplyCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      eModeLtv: 1,
+      eModeLiquidationThreshold: 1,
+      eModeLiquidationBonus: 1,
+      eModePriceSource: '0x3E0437898a5667a4769B1Ca5A34aAB1ae7E81377',
+      eModeLabel: 'test label',
+    },
+  ],
+  1: {
+    marketReferenceCurrencyUnit: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    marketReferenceCurrencyPriceInUsd: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+
+    networkBaseTokenPriceInUsd: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    networkBaseTokenPriceDecimals: 0,
+  },
+};
+
+export const userReservesMock: UserReserveData = {
+  0: [
+    {
+      underlyingAsset: '0xB597cd8D3217ea6477232F9217fa70837ff667Af',
+      scaledATokenBalance: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      usageAsCollateralEnabledOnUser: false,
+      stableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      scaledVariableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      principalStableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      stableBorrowLastUpdateTimestamp: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+    },
+  ],
+  1: 1,
+};
