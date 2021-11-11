4724a4e4208a110f21e267aa01280b673da9e01f
sendra <polsendra@gmail.com>
2021-11-11 15:39:43 +0100

    feat: added explicit v3 exports

--- packages/contract-helpers/src/index.ts
+++ packages/contract-helpers/src/index.ts
@@ -5,6 +5,17 @@ export {
 } from './permissions-manager/types/PermissionManagerTypes';
 export * from './ui-incentive-data-provider';
 export * from './ui-pool-data-provider';
+export {
+  UiPoolDataProvider as UiPoolDataProviderV3,
+  UiPoolDataProviderInterface as UiPoolDataProviderInterfaceV3,
+  UiPoolDataProviderContext as UiPoolDataProviderContextV3,
+  ReservesData as ReservesDataV3,
+  UserReserveData as UserReserveDataV3,
+  PoolBaseCurrencyHumanized as PoolBaseCurrencyHumanizedV3,
+  ReserveDataHumanized as ReserveDataHumanizedV3,
+  ReservesDataHumanized as ReservesDataHumanizedV3,
+  UserReserveDataHumanized as UserReserveDataHumanizedV3,
+} from './v3-UiPoolDataProvider-contract';
 export * from './wallet-balance-provider';
 export * from './cl-feed-registry';
 
