ba4ee412289193e391e1c8606d68cd3813678893
sendra <polsendra@gmail.com>
2021-11-12 13:33:47 +0100

    Revert "fix: removed export of v2"
    This reverts commit 7c3129ee5de1de01e22beb075d5d0bd40741e552.

--- packages/contract-helpers/src/index.ts
+++ packages/contract-helpers/src/index.ts
@@ -4,30 +4,29 @@ export {
   PERMISSION_MAP,
 } from './permissions-manager/types/PermissionManagerTypes';
 export * from './ui-incentive-data-provider';
-export * from './v3-UiPoolDataProvider-contract';
-// export * from './ui-pool-data-provider';
-// import {
-//   UiPoolDataProvider,
-//   UiPoolDataProviderInterface,
-//   UiPoolDataProviderContext,
-//   ReservesData,
-//   UserReserveData,
-//   PoolBaseCurrencyHumanized,
-//   ReserveDataHumanized,
-//   ReservesDataHumanized,
-//   UserReserveDataHumanized,
-// } from './v3-UiPoolDataProvider-contract';
-// export {
-//   UiPoolDataProvider as UiPoolDataProviderV3,
-//   UiPoolDataProviderInterface as UiPoolDataProviderInterfaceV3,
-//   UiPoolDataProviderContext as UiPoolDataProviderContextV3,
-//   ReservesData as ReservesDataV3,
-//   UserReserveData as UserReserveDataV3,
-//   PoolBaseCurrencyHumanized as PoolBaseCurrencyHumanizedV3,
-//   ReserveDataHumanized as ReserveDataHumanizedV3,
-//   ReservesDataHumanized as ReservesDataHumanizedV3,
-//   UserReserveDataHumanized as UserReserveDataHumanizedV3,
-// };
+export * from './ui-pool-data-provider';
+import {
+  UiPoolDataProvider,
+  UiPoolDataProviderInterface,
+  UiPoolDataProviderContext,
+  ReservesData,
+  UserReserveData,
+  PoolBaseCurrencyHumanized,
+  ReserveDataHumanized,
+  ReservesDataHumanized,
+  UserReserveDataHumanized,
+} from './v3-UiPoolDataProvider-contract';
+export {
+  UiPoolDataProvider as UiPoolDataProviderV3,
+  UiPoolDataProviderInterface as UiPoolDataProviderInterfaceV3,
+  UiPoolDataProviderContext as UiPoolDataProviderContextV3,
+  ReservesData as ReservesDataV3,
+  UserReserveData as UserReserveDataV3,
+  PoolBaseCurrencyHumanized as PoolBaseCurrencyHumanizedV3,
+  ReserveDataHumanized as ReserveDataHumanizedV3,
+  ReservesDataHumanized as ReservesDataHumanizedV3,
+  UserReserveDataHumanized as UserReserveDataHumanizedV3,
+};
 export * from './wallet-balance-provider';
 export * from './cl-feed-registry';
 
