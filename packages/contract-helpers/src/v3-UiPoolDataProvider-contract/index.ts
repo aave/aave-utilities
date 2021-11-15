47f2b60415645da0e9c58bf13205ce8d2754ca4f
sendra <polsendra@gmail.com>
2021-11-15 11:31:26 +0100

    feat: clean v3 pool provider changes

--- packages/contract-helpers/src/v3-UiPoolDataProvider-contract/index.ts
+++ packages/contract-helpers/src/v3-UiPoolDataProvider-contract/index.ts
@@ -179,7 +179,11 @@ export class UiPoolDataProvider implements UiPoolDataProviderInterface {
         eModeLiquidationThreshold: reserveRaw.eModeLiquidationThreshold,
         eModeLiquidationBonus: reserveRaw.eModeLiquidationBonus,
         eModePriceSource: reserveRaw.eModePriceSource.toString(),
-        eModeLabel: reserveRaw.eModeLabel,
+        eModeLabel: reserveRaw.eModeLabel.toString(),
+        accruedToTreasury: reserveRaw.accruedToTreasury.toString(),
+        unbacked: reserveRaw.unbacked.toString(),
+        isolationModeTotalDebt: reserveRaw.isolationModeTotalDebt.toString(),
+        debtCeilingDecimals: reserveRaw.debtCeiling.toNumber(),
       }),
     );
 
