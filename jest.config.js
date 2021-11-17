4cf6eea7c86368afe6f6ae497464ed05cc7c2e22
sendra <polsendra@gmail.com>
2021-11-17 15:43:00 +0100

    fix: added typechain to ignore path

--- jest.config.js
+++ jest.config.js
@@ -35,6 +35,7 @@ module.exports = {
     'packages/contract-helpers/src/governance-contract/typechain',
     'packages/contract-helpers/src/governance-power-delegation-contract/typechain',
     'packages/contract-helpers/src/v3-pool-contract/typechain',
+    'packages/contract-helpers/src/erc20_2612/typechain',
     'packages/contract-helpers/src/index.ts',
   ],
   modulePathIgnorePatterns: ['node_modules'],
