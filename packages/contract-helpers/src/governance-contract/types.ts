e279e8152d9e32836bb8d0bcffdfa08363483fbb
lukas strassel <lukasstrassel@googlemail.com>
2021-11-19 21:43:33 +0100

    fix: drop ipfs fetching

--- packages/contract-helpers/src/governance-contract/types.ts
+++ packages/contract-helpers/src/governance-contract/types.ts
@@ -88,11 +88,37 @@ export enum ProposalState {
   Executed = 'Executed',
 }
 
-export type Proposal = {
+export type ProposalIpfs = {
   id: number;
   title: string;
   description: string;
   shortDescription: string;
+  creator: tEthereumAddress;
+  executor: tEthereumAddress;
+  targets: tEthereumAddress[];
+  values: BigNumber[];
+  signatures: string[];
+  calldatas: string[];
+  withDelegatecalls: boolean[];
+  startBlock: number;
+  endBlock: number;
+  executionTime: string;
+  executionTimeWithGracePeriod: string;
+  forVotes: string;
+  againstVotes: string;
+  executed: boolean;
+  canceled: boolean;
+  strategy: string;
+  ipfsHash: string;
+  state: ProposalState;
+  minimumQuorum: string;
+  minimumDiff: string;
+  proposalCreated: number;
+  totalVotingSupply: string;
+};
+
+export type Proposal = {
+  id: number;
   creator: tEthereumAddress;
   executor: tEthereumAddress;
   targets: tEthereumAddress[];
