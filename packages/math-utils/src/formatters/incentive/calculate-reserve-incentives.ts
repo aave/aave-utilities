27a938b6af6af3a82d380ee67b39da8a597f2f38
Andrew Schmidt <andrew@aave.com>
2021-12-01 00:07:36 -0600

    feat: handle multi-incentives for reserves

--- packages/math-utils/src/formatters/incentive/calculate-reserve-incentives.ts
+++ packages/math-utils/src/formatters/incentive/calculate-reserve-incentives.ts
@@ -1,11 +1,8 @@
 import { calculateIncentiveAPR } from './calculate-incentive-apr';
-import { ReserveIncentiveWithFeedsResponse } from './types';
+import { ReservesIncentiveDataHumanized } from './types';
 
 export interface CalculateReserveIncentivesRequest {
-  reserveIncentiveData: ReserveIncentiveWithFeedsResponse;
-  aRewardTokenPriceInMarketReferenceCurrency: string; // Can be priced in ETH or USD depending on market
-  vRewardTokenPriceInMarketReferenceCurrency: string;
-  sRewardTokenPriceInMarketReferenceCurrency: string;
+  reserveIncentiveData: ReservesIncentiveDataHumanized;
   totalLiquidity: string;
   totalVariableDebt: string;
   totalStableDebt: string;
@@ -16,75 +13,80 @@ export interface CalculateReserveIncentivesRequest {
 interface ReserveIncentiveResponse {
   incentiveAPR: string;
   rewardTokenAddress: string;
+  rewardTokenSymbol: string;
 }
 export interface CalculateReserveIncentivesResponse {
   underlyingAsset: string;
-  aIncentivesData: ReserveIncentiveResponse;
-  vIncentivesData: ReserveIncentiveResponse;
-  sIncentivesData: ReserveIncentiveResponse;
+  aIncentivesData: ReserveIncentiveResponse[];
+  vIncentivesData: ReserveIncentiveResponse[];
+  sIncentivesData: ReserveIncentiveResponse[];
 }
 
 // Calculate deposit, variableBorrow, and stableBorrow incentives APR for a reserve asset
 export function calculateReserveIncentives({
   reserveIncentiveData,
-  aRewardTokenPriceInMarketReferenceCurrency,
-  vRewardTokenPriceInMarketReferenceCurrency,
-  sRewardTokenPriceInMarketReferenceCurrency,
   totalLiquidity,
   totalVariableDebt,
   totalStableDebt,
   decimals,
   priceInMarketReferenceCurrency,
 }: CalculateReserveIncentivesRequest): CalculateReserveIncentivesResponse {
-  const aIncentivesAPR = calculateIncentiveAPR({
-    emissionPerSecond: reserveIncentiveData.aIncentiveData.emissionPerSecond,
-    rewardTokenPriceInMarketReferenceCurrency:
-      aRewardTokenPriceInMarketReferenceCurrency,
-    priceInMarketReferenceCurrency,
-    totalTokenSupply: totalLiquidity,
-    decimals,
-    rewardTokenDecimals:
-      reserveIncentiveData.aIncentiveData.rewardTokenDecimals,
-  });
-
-  const vIncentivesAPR = calculateIncentiveAPR({
-    emissionPerSecond: reserveIncentiveData.vIncentiveData.emissionPerSecond,
-    rewardTokenPriceInMarketReferenceCurrency:
-      vRewardTokenPriceInMarketReferenceCurrency,
-    priceInMarketReferenceCurrency,
-    totalTokenSupply: totalVariableDebt,
-    decimals,
-    rewardTokenDecimals:
-      reserveIncentiveData.aIncentiveData.rewardTokenDecimals,
-  });
-
-  const sIncentivesAPR = calculateIncentiveAPR({
-    emissionPerSecond: reserveIncentiveData.sIncentiveData.emissionPerSecond,
-    rewardTokenPriceInMarketReferenceCurrency:
-      sRewardTokenPriceInMarketReferenceCurrency,
-    priceInMarketReferenceCurrency,
-    totalTokenSupply: totalStableDebt,
-    decimals,
-    rewardTokenDecimals:
-      reserveIncentiveData.aIncentiveData.rewardTokenDecimals,
-  });
+  const aIncentivesData: ReserveIncentiveResponse[] =
+    reserveIncentiveData.aIncentiveData.rewardsTokenInformation.map(reward => {
+      const aIncentivesAPR = calculateIncentiveAPR({
+        emissionPerSecond: reward.emissionPerSecond,
+        rewardTokenPriceInMarketReferenceCurrency: reward.rewardPriceFeed,
+        priceInMarketReferenceCurrency,
+        totalTokenSupply: totalLiquidity,
+        decimals,
+        rewardTokenDecimals: reward.rewardTokenDecimals,
+      });
+      const aIncentiveData: ReserveIncentiveResponse = {
+        incentiveAPR: aIncentivesAPR,
+        rewardTokenAddress: reward.rewardTokenAddress,
+        rewardTokenSymbol: reward.rewardTokenSymbol,
+      };
+      return aIncentiveData;
+    });
+  const vIncentivesData: ReserveIncentiveResponse[] =
+    reserveIncentiveData.aIncentiveData.rewardsTokenInformation.map(reward => {
+      const vIncentivesAPR = calculateIncentiveAPR({
+        emissionPerSecond: reward.emissionPerSecond,
+        rewardTokenPriceInMarketReferenceCurrency: reward.rewardPriceFeed,
+        priceInMarketReferenceCurrency,
+        totalTokenSupply: totalVariableDebt,
+        decimals,
+        rewardTokenDecimals: reward.rewardTokenDecimals,
+      });
+      const vIncentiveData: ReserveIncentiveResponse = {
+        incentiveAPR: vIncentivesAPR,
+        rewardTokenAddress: reward.rewardTokenAddress,
+        rewardTokenSymbol: reward.rewardTokenSymbol,
+      };
+      return vIncentiveData;
+    });
+  const sIncentivesData: ReserveIncentiveResponse[] =
+    reserveIncentiveData.aIncentiveData.rewardsTokenInformation.map(reward => {
+      const sIncentivesAPR = calculateIncentiveAPR({
+        emissionPerSecond: reward.emissionPerSecond,
+        rewardTokenPriceInMarketReferenceCurrency: reward.rewardPriceFeed,
+        priceInMarketReferenceCurrency,
+        totalTokenSupply: totalStableDebt,
+        decimals,
+        rewardTokenDecimals: reward.rewardTokenDecimals,
+      });
+      const sIncentiveData: ReserveIncentiveResponse = {
+        incentiveAPR: sIncentivesAPR,
+        rewardTokenAddress: reward.rewardTokenAddress,
+        rewardTokenSymbol: reward.rewardTokenSymbol,
+      };
+      return sIncentiveData;
+    });
 
   return {
     underlyingAsset: reserveIncentiveData.underlyingAsset,
-    aIncentivesData: {
-      incentiveAPR: aIncentivesAPR,
-      rewardTokenAddress:
-        reserveIncentiveData.aIncentiveData.rewardTokenAddress,
-    },
-    vIncentivesData: {
-      incentiveAPR: vIncentivesAPR,
-      rewardTokenAddress:
-        reserveIncentiveData.vIncentiveData.rewardTokenAddress,
-    },
-    sIncentivesData: {
-      incentiveAPR: sIncentivesAPR,
-      rewardTokenAddress:
-        reserveIncentiveData.sIncentiveData.rewardTokenAddress,
-    },
+    aIncentivesData,
+    vIncentivesData,
+    sIncentivesData,
   };
 }
