2a4fab4cad7405293a37096b7ad22ea4c78da68c
Andrew Schmidt <andrew@aave.com>
2021-12-01 00:06:36 -0600

    feat: update UiIncentiveDataProvider types

--- packages/math-utils/src/formatters/incentive/types.ts
+++ packages/math-utils/src/formatters/incentive/types.ts
@@ -1,43 +1,52 @@
 // From UiIncentiveDatProvider
-export interface ReserveIncentiveWithFeedsResponse {
+export interface ReservesIncentiveDataHumanized {
   underlyingAsset: string;
-  aIncentiveData: IncentivesWithFeeds;
-  vIncentiveData: IncentivesWithFeeds;
-  sIncentiveData: IncentivesWithFeeds;
-}
-
-export interface IncentivesWithFeeds extends IncentiveDataHumanized {
-  priceFeed: string;
-  priceFeedTimestamp: number;
-  priceFeedDecimals: number;
+  aIncentiveData: IncentiveDataHumanized;
+  vIncentiveData: IncentiveDataHumanized;
+  sIncentiveData: IncentiveDataHumanized;
 }
 
 export interface IncentiveDataHumanized {
+  tokenAddress: string;
+  incentiveControllerAddress: string;
+  rewardsTokenInformation: RewardInfoHumanized[];
+}
+
+export interface RewardInfoHumanized {
+  rewardTokenSymbol: string;
+  rewardTokenAddress: string;
+  rewardOracleAddress: string;
   emissionPerSecond: string;
   incentivesLastUpdateTimestamp: number;
   tokenIncentivesIndex: string;
   emissionEndTimestamp: number;
-  tokenAddress: string;
-  rewardTokenAddress: string;
-  incentiveControllerAddress: string;
+  rewardPriceFeed: string;
   rewardTokenDecimals: number;
   precision: number;
+  priceFeedDecimals: number;
 }
 
-// From UiIncentiveDataProvider
-export interface UserReserveIncentiveDataHumanizedResponse {
+export interface UserReservesIncentivesDataHumanized {
   underlyingAsset: string;
-  aTokenIncentivesUserData: IncentiveUserDataHumanized;
-  vTokenIncentivesUserData: IncentiveUserDataHumanized;
-  sTokenIncentivesUserData: IncentiveUserDataHumanized;
+  aTokenIncentivesUserData: UserIncentiveDataHumanized;
+  vTokenIncentivesUserData: UserIncentiveDataHumanized;
+  sTokenIncentivesUserData: UserIncentiveDataHumanized;
 }
 
-export interface IncentiveUserDataHumanized {
-  tokenIncentivesUserIndex: string;
-  userUnclaimedRewards: string;
+export interface UserIncentiveDataHumanized {
   tokenAddress: string;
-  rewardTokenAddress: string;
   incentiveControllerAddress: string;
+  userRewardsInformation: UserRewardInfoHumanized[];
+}
+
+export interface UserRewardInfoHumanized {
+  rewardTokenSymbol: string;
+  rewardOracleAddress: string;
+  rewardTokenAddress: string;
+  userUnclaimedRewards: string;
+  tokenIncentivesUserIndex: string;
+  rewardPriceFeed: string;
+  priceFeedDecimals: number;
   rewardTokenDecimals: number;
 }
 
@@ -61,4 +70,49 @@ export interface ReserveCalculationData {
   priceInMarketReferenceCurrency: string;
   marketReferenceCurrencyDecimals: number;
   decimals: number;
+}
+
+// OLD INCENTIVES TYPES BELOW, WILLBE REMOVED
+
+// From UiIncentiveDatProvider
+export interface ReserveIncentiveWithFeedsResponse {
+  underlyingAsset: string;
+  aIncentiveData: IncentivesWithFeeds;
+  vIncentiveData: IncentivesWithFeeds;
+  sIncentiveData: IncentivesWithFeeds;
+}
+
+export interface IncentivesWithFeeds extends IncentiveDataHumanizedOld {
+  priceFeed: string;
+  priceFeedTimestamp: number;
+  priceFeedDecimals: number;
+}
+
+export interface IncentiveDataHumanizedOld {
+  emissionPerSecond: string;
+  incentivesLastUpdateTimestamp: number;
+  tokenIncentivesIndex: string;
+  emissionEndTimestamp: number;
+  tokenAddress: string;
+  rewardTokenAddress: string;
+  incentiveControllerAddress: string;
+  rewardTokenDecimals: number;
+  precision: number;
+}
+
+// From UiIncentiveDataProvider
+export interface UserReserveIncentiveDataHumanizedResponse {
+  underlyingAsset: string;
+  aTokenIncentivesUserData: IncentiveUserDataHumanizedOld;
+  vTokenIncentivesUserData: IncentiveUserDataHumanizedOld;
+  sTokenIncentivesUserData: IncentiveUserDataHumanizedOld;
+}
+
+export interface IncentiveUserDataHumanizedOld {
+  tokenIncentivesUserIndex: string;
+  userUnclaimedRewards: string;
+  tokenAddress: string;
+  rewardTokenAddress: string;
+  incentiveControllerAddress: string;
+  rewardTokenDecimals: number;
 }
