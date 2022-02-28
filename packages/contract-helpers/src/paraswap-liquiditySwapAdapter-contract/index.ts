4947b6a6a650192d3553369ebe6636859d71ead0
Lukas <lukasstrassel@googlemail.com>
2022-02-28 21:26:31 +0100

    fix: paraswap univ2 router

--- packages/contract-helpers/src/paraswap-liquiditySwapAdapter-contract/index.ts
+++ packages/contract-helpers/src/paraswap-liquiditySwapAdapter-contract/index.ts
@@ -32,12 +32,29 @@ export function augustusFromAmountOffsetFromCalldata(calldata: string): number {
       return 4; // 4 + 0 * 32
     case '0xf5661034': // Augustus V5 swapOnUniswapFork
       return 68; // 4 + 2 * 32
+    case '0x0b86a4c1': // Augustus V5 swapOnUniswapV2Fork
+      return 36; // 4 + 1 * 32
     case '0x64466805': // Augustus V5 swapOnZeroXv4
       return 68; // 4 + 2 * 32
     case '0xa94e78ef': // Augustus V5 multiSwap
       return 68; // 4 + 2 * 32
     case '0x46c67b6d': // Augustus V5 megaSwap
       return 68; // 4 + 2 * 32
+    default:
+      throw new Error('Unrecognized function selector for Augustus');
+  }
+}
+
+export function augustusToAmountOffsetFromCalldata(calldata: string) {
+  switch (calldata.slice(0, 10)) {
+    case '0x935fb84b': // Augustus V5 buyOnUniswap
+      return 36; // 4 + 1 * 32
+    case '0xc03786b0': // Augustus V5 buyOnUniswapFork
+      return 100; // 4 + 3 * 32
+    case '0xb2f1e6db': // Augustus V5 buyOnUniswapV2Fork
+      return 68; // 4 + 2 * 32
+    case '0xb66bcbac': // Augustus V5 buy
+      return 164; // 4 + 5 * 32
     default:
       throw new Error('Unrecognized function selector for Augustus');
   }
