6ea078aaa66d7d942cc7f7a4cd68fb9fda0ac123
Andrew Schmidt <andrew@aave.com>
2022-12-12 16:12:51 -0600

    chore: purge unused gho services

--- packages/contract-helpers/src/gho/__tests__/index.test.ts
+++ packages/contract-helpers/src/gho/__tests__/index.test.ts
@@ -1,23 +1,6 @@
-import {
-  GhoDiscountRateStrategyService,
-  GhoTokenService,
-  GhoVariableDebtTokenService,
-  GhoService,
-} from '../index';
+import { GhoService } from '../index';
 
 describe('gho contract helpers', () => {
-  it('exports out the GhoDiscountRateStrategyService', () => {
-    expect(GhoDiscountRateStrategyService).toBeTruthy();
-  });
-
-  it('exports out the GhoTokenService', () => {
-    expect(GhoTokenService).toBeTruthy();
-  });
-
-  it('exports out the GhoVariableDebtTokenService', () => {
-    expect(GhoVariableDebtTokenService).toBeTruthy();
-  });
-
   it('exports out the GhoService', () => {
     expect(GhoService).toBeTruthy();
   });
--- packages/contract-helpers/src/gho/index.ts
+++ packages/contract-helpers/src/gho/index.ts
@@ -1,11 +1,3 @@
-import { GhoDiscountRateStrategyService } from './GhoDiscountRateStrategyService';
 import { GhoService } from './GhoService';
-import { GhoTokenService } from './GhoTokenService';
-import { GhoVariableDebtTokenService } from './GhoVariableDebtTokenService';
 
-export {
-  GhoDiscountRateStrategyService,
-  GhoTokenService,
-  GhoVariableDebtTokenService,
-  GhoService,
-};
+export { GhoService };
