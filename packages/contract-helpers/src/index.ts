160839863a83955d54a4a1e727bb4a22f111c13e
David K <canillas.mail@gmail.com>
2023-01-03 16:26:30 +0100

    feat: add V3Faucet service (#475)

--- packages/contract-helpers/src/index.ts
+++ packages/contract-helpers/src/index.ts
@@ -15,6 +15,7 @@ export * from './incentive-controller-v2';
 export * from './erc20-contract';
 export * from './lendingPool-contract';
 export * from './faucet-contract';
+export * from './v3-faucet-contract';
 export * from './staking-contract';
 export * from './governance-contract';
 export * from './governance-contract/types';
