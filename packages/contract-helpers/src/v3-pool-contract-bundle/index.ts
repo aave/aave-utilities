dd4ce834684a019658d304399591953d09811c01
aschmidt20 <andrew@aave.com>
2023-03-21 00:21:56 -0500

    chore: format

--- packages/contract-helpers/src/v3-pool-contract-bundle/index.ts
+++ packages/contract-helpers/src/v3-pool-contract-bundle/index.ts
@@ -1,24 +1,11 @@
 import { Signature, splitSignature } from '@ethersproject/bytes';
 import { BigNumber, PopulatedTransaction, providers } from 'ethers';
 import BaseService from '../commons/BaseService';
-import {
-  ActionBundle,
-  DEFAULT_DEADLINE,
-  ProtocolAction,
-  SignedActionRequest,
-} from '../commons/types';
+import { DEFAULT_DEADLINE, ProtocolAction } from '../commons/types';
 import {
   API_ETH_MOCK_ADDRESS,
-  convertPopulatedTx,
-  DEFAULT_APPROVE_AMOUNT,
   gasLimitRecommendations,
-  valueToWei,
 } from '../commons/utils';
-import { LPValidatorV3 } from '../commons/validators/methodValidators';
-import {
-  isEthAddress,
-  isPositiveAmount,
-} from '../commons/validators/paramValidators';
 import { ERC20_2612Service, ERC20_2612Interface } from '../erc20-2612';
 import {
   ApproveType,
@@ -89,15 +76,8 @@ export type SupplyTxBuilder = {
 };
 
 export interface PoolBundleInterface {
-  supplyBundle: (args: LPSupplyParamsBundleType) => Promise<ActionBundle>;
   supplyTxBuilder: SupplyTxBuilder;
 }
-
-export type LPSupplyParamsBundleType = LPSupplyParamsType & {
-  skipApprovalChecks?: boolean;
-  skipGasEstimation?: boolean;
-  deadline?: string;
-};
 
 export class PoolBundle
   extends BaseService<IPool>
@@ -290,193 +270,6 @@ export class PoolBundle
         });
         return txGas.gasLimit ?? BigNumber.from('0');
       },
-    };
-  }
-
-  @LPValidatorV3
-  public async supplyBundle(
-    @isEthAddress('user')
-    @isEthAddress('reserve')
-    @isPositiveAmount('amount')
-    @isEthAddress('onBehalfOf')
-    {
-      user,
-      reserve,
-      amount,
-      onBehalfOf,
-      referralCode,
-      useOptimizedPath,
-      skipApprovalChecks,
-      skipGasEstimation,
-      deadline,
-    }: LPSupplyParamsBundleType,
-  ): Promise<ActionBundle> {
-    let actionTx: PopulatedTransaction = {};
-    let approved = true;
-    const approvals: PopulatedTransaction[] = [];
-    const signatureRequests: string[] = [];
-
-    const generateSignedAction = async ({
-      signatures,
-    }: SignedActionRequest) => {
-      const { decimalsOf }: IERC20ServiceInterface = this.erc20Service;
-      const convertedAmount: string = valueToWei(
-        amount,
-        await decimalsOf(reserve),
-      );
-      const sig: Signature = splitSignature(signatures[0]);
-      const fundsAvailable: boolean =
-        await this.synthetixService.synthetixValidation({
-          user,
-          reserve,
-          amount: convertedAmount,
-        });
-      if (!fundsAvailable) {
-        throw new Error('Not enough funds to execute operation');
-      }
-
-      let populatedTx: PopulatedTransaction = {};
-      if (useOptimizedPath) {
-        const legacyTx = await this.l2PoolService.supplyWithPermit(
-          {
-            user,
-            reserve,
-            amount: convertedAmount,
-            referralCode,
-            deadline: DEFAULT_DEADLINE,
-            permitV: sig.v,
-            permitR: sig.r,
-            permitS: sig.s,
-          },
-          [],
-        );
-        const executedTx = await legacyTx[0].tx();
-        populatedTx = convertPopulatedTx(executedTx);
-      } else {
-        const txData = this.contractInterface.encodeFunctionData(
-          'supplyWithPermit',
-          [
-            reserve,
-            convertedAmount,
-            onBehalfOf ?? user,
-            referralCode ?? '0',
-            DEFAULT_DEADLINE,
-            sig.v,
-            sig.r,
-            sig.s,
-          ],
-        );
-        populatedTx.to = this.poolAddress;
-        populatedTx.from = user;
-        populatedTx.data = txData;
-      }
-
-      populatedTx = await this.estimateGasLimit({
-        tx: populatedTx,
-        action: ProtocolAction.supplyWithPermit,
-        skipGasEstimation,
-      });
-
-      return populatedTx;
-    };
-
-    // Base asset supplies are routed to WETHGateway
-    if (reserve.toLowerCase() === API_ETH_MOCK_ADDRESS.toLowerCase()) {
-      const depositEth = this.wethGatewayService.depositETH({
-        lendingPool: this.poolAddress,
-        user,
-        amount,
-        onBehalfOf,
-        referralCode,
-      });
-      const txData = await depositEth[0].tx();
-      actionTx = convertPopulatedTx(txData);
-
-      actionTx = await this.estimateGasLimit({
-        tx: actionTx,
-        action: ProtocolAction.supply,
-        skipGasEstimation,
-      });
-    } else {
-      // Handle mon-base asset supplies
-      const { isApproved, decimalsOf, approveTxData }: IERC20ServiceInterface =
-        this.erc20Service;
-      const reserveDecimals: number = await decimalsOf(reserve);
-      const convertedAmount: string = valueToWei(amount, reserveDecimals);
-
-      const fundsAvailable: boolean =
-        await this.synthetixService.synthetixValidation({
-          user,
-          reserve,
-          amount: convertedAmount,
-        });
-      if (!fundsAvailable) {
-        throw new Error('Not enough funds to execute operation');
-      }
-
-      // Generate approval and signature requests, optional
-      if (!skipApprovalChecks) {
-        approved = await isApproved({
-          token: reserve,
-          user,
-          spender: this.poolAddress,
-          amount,
-        });
-
-        if (!approved) {
-          const approveTx = approveTxData({
-            user,
-            token: reserve,
-            spender: this.poolAddress,
-            amount: DEFAULT_APPROVE_AMOUNT,
-          });
-
-          const signatureRequest = await this.v3PoolService.signERC20Approval({
-            user,
-            reserve,
-            amount,
-            deadline: deadline ?? DEFAULT_DEADLINE,
-          });
-          approvals.push(approveTx);
-          signatureRequests.push(signatureRequest);
-        }
-      }
-
-      // compress calldata for L2Pool
-      if (useOptimizedPath) {
-        const legacyL2SupplyTx = await this.l2PoolService.supply(
-          { user, reserve, amount: convertedAmount, referralCode },
-          [],
-        );
-        const executedTx = await legacyL2SupplyTx[0].tx();
-        actionTx = convertPopulatedTx(executedTx);
-      } else {
-        const txData = this.contractInterface.encodeFunctionData('supply', [
-          reserve,
-          convertedAmount,
-          onBehalfOf ?? user,
-          referralCode ?? '0',
-        ]);
-        actionTx.to = this.poolAddress;
-        actionTx.from = user;
-        actionTx.data = txData;
-      }
-
-      actionTx = await this.estimateGasLimit({
-        tx: actionTx,
-        action: ProtocolAction.supply,
-        skipGasEstimation,
-      });
-    }
-
-    return {
-      action: actionTx,
-      approvalRequired: !approved,
-      approvals,
-      signatureRequests,
-      generateSignedAction,
-      signedActionGasEstimate:
-        gasLimitRecommendations[ProtocolAction.supplyWithPermit].limit,
     };
   }
 }
