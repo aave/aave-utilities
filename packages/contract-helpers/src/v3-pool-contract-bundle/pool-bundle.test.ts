cd1ad0be5eb6d832b929118399dc719e589fa1f8
aschmidt20 <andrew@aave.com>
2023-03-22 23:35:01 -0500

    chore: update L2Pool tests for SupplyTxBuilder + full test coverage

--- packages/contract-helpers/src/v3-pool-contract-bundle/pool-bundle.test.ts
+++ packages/contract-helpers/src/v3-pool-contract-bundle/pool-bundle.test.ts
@@ -109,9 +109,22 @@ describe('PoolBundle', () => {
         referralCode: '0',
       });
 
+      const differentParamsSameResult = instance.supplyTxBuilder.generateTxData(
+        {
+          user: USER,
+          reserve: TOKEN,
+          amount: '1',
+        },
+      );
+
       expect(result.to).toEqual(POOL);
       expect(result.from).toEqual(USER);
       expect(result.data).toEqual(
+        '0x617ba0370000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000',
+      );
+      expect(differentParamsSameResult.to).toEqual(POOL);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
         '0x617ba0370000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000',
       );
     });
@@ -124,10 +137,23 @@ describe('PoolBundle', () => {
         onBehalfOf: USER,
         referralCode: '0',
       });
+
+      const differentParamsSameResult = instance.supplyTxBuilder.generateTxData(
+        {
+          user: USER,
+          reserve: API_ETH_MOCK_ADDRESS.toLowerCase(),
+          amount: '1',
+        },
+      );
       expect(result.to).toEqual(WETH_GATEWAY);
       expect(result.from).toEqual(USER);
       expect(result.value).toEqual(BigNumber.from('1'));
       expect(result.data).toEqual(
+        '0x474cf53d000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000',
+      );
+      expect(differentParamsSameResult.to).toEqual(WETH_GATEWAY);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
         '0x474cf53d000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000',
       );
     });
@@ -142,10 +168,60 @@ describe('PoolBundle', () => {
         useOptimizedPath: true,
       });
 
+      const differentParamsSameResult = instance.supplyTxBuilder.generateTxData(
+        {
+          user: USER,
+          reserve: TOKEN,
+          amount: '1',
+          useOptimizedPath: true,
+        },
+      );
+
       expect(result.to).toEqual(POOL);
       expect(result.from).toEqual(USER);
       expect(result.data).toEqual(
         '0x617ba0370000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000',
+      );
+      expect(differentParamsSameResult.to).toEqual(POOL);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
+        '0x617ba0370000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000030000000000000000000000000000000000000000000000000000000000000000',
+      );
+    });
+
+    it('generates supply tx data with generateTxData and L2POOL with encoded txData', () => {
+      const result = instance.supplyTxBuilder.generateTxData({
+        user: USER,
+        reserve: TOKEN,
+        amount: '1',
+        onBehalfOf: USER,
+        referralCode: '0',
+        useOptimizedPath: true,
+        encodedTxData:
+          '0x0000000000000000000000000000000000000000000000000000006d6168616d',
+      });
+
+      const differentParamsSameResult = instance.supplyTxBuilder.generateTxData(
+        {
+          user: USER,
+          reserve: TOKEN,
+          amount: '1',
+          useOptimizedPath: true,
+          onBehalfOf: USER,
+          encodedTxData:
+            '0x0000000000000000000000000000000000000000000000000000006d6168616d',
+        },
+      );
+
+      expect(result.to).toEqual(POOL);
+      expect(result.from).toEqual(USER);
+      expect(result.data).toEqual(
+        '0xf7a738400000000000000000000000000000000000000000000000000000006d6168616d',
+      );
+      expect(differentParamsSameResult.to).toEqual(POOL);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
+        '0xf7a738400000000000000000000000000000000000000000000000000000006d6168616d',
       );
     });
 
@@ -161,9 +237,25 @@ describe('PoolBundle', () => {
         deadline: '10000',
       });
 
+      const differentParamsSameResult =
+        instance.supplyTxBuilder.generateSignedTxData({
+          user: USER,
+          reserve: TOKEN,
+          amount: '1',
+
+          signature:
+            '0x532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb41c',
+          deadline: '10000',
+        });
+
       expect(result.to).toEqual(POOL);
       expect(result.from).toEqual(USER);
       expect(result.data).toEqual(
+        '0x02c205f000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000001c532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb4',
+      );
+      expect(differentParamsSameResult.to).toEqual(POOL);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
         '0x02c205f000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000001c532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb4',
       );
     });
@@ -181,10 +273,68 @@ describe('PoolBundle', () => {
         useOptimizedPath: true,
       });
 
+      const differentParamsSameResult =
+        instance.supplyTxBuilder.generateSignedTxData({
+          user: USER,
+          reserve: TOKEN,
+          amount: '1',
+          signature:
+            '0x532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb41c',
+          deadline: '10000',
+          useOptimizedPath: true,
+        });
+
       expect(result.to).toEqual(POOL);
       expect(result.from).toEqual(USER);
       expect(result.data).toEqual(
         '0x02c205f000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000001c532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb4',
+      );
+      expect(differentParamsSameResult.to).toEqual(POOL);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
+        '0x02c205f000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000300000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000002710000000000000000000000000000000000000000000000000000000000000001c532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb4',
+      );
+    });
+
+    it('generates signed tx with generateSignedTxData with L2POOL with encoded txData', () => {
+      const result = instance.supplyTxBuilder.generateSignedTxData({
+        user: USER,
+        reserve: TOKEN,
+        amount: '1',
+        onBehalfOf: USER,
+        referralCode: '0',
+        signature:
+          '0x532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb41c',
+        deadline: '10000',
+        useOptimizedPath: true,
+        encodedTxData:
+          '0x0000000000000000000000000000000000000000000000000000006d6168616d',
+      });
+
+      const differentParamsSameResult =
+        instance.supplyTxBuilder.generateSignedTxData({
+          user: USER,
+          reserve: TOKEN,
+          amount: '1',
+          referralCode: '0',
+          signature:
+            '0x532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb41c',
+          deadline: '10000',
+          useOptimizedPath: true,
+          encodedTxData:
+            '0x0000000000000000000000000000000000000000000000000000006d6168616d',
+        });
+
+      expect(result.to).toEqual(POOL);
+      expect(result.from).toEqual(USER);
+      expect(result.data).toEqual(
+        '0x680dd47c0000000000000000000000000000000000000000000000000000006d6168616d532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb4',
+      );
+
+      expect(differentParamsSameResult.to).toEqual(POOL);
+      expect(differentParamsSameResult.from).toEqual(USER);
+      expect(differentParamsSameResult.data).toEqual(
+        '0x680dd47c0000000000000000000000000000000000000000000000000000006d6168616d532f8df4e2502bd869fb35e9301156f9b307380afdcc25cfbc87b2e939f16f7e47c326dc26eb918d327358797ee67ad7415d871ef7eaf0d4f6352d3ad021fbb4',
       );
     });
   });
