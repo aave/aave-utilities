1a26dd67a2c9f5674bd83b0bdfeb620750b62b96
aschmidt20 <andrew@aave.com>
2023-03-29 16:41:00 -0500

    chore: remove to re-trigger deploy

--- packages/contract-helpers/src/v3-migration-contract/index.ts
+++ packages/contract-helpers/src/v3-migration-contract/index.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import BigNumber from 'bignumber.js';
 import { constants, providers, utils } from 'ethers';
 import {
