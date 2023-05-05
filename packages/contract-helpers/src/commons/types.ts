21fcb7b57bb6c7a1edd3f0d6481fd12e1e10c084
Andrew Schmidt <andrew@aave.com>
2023-05-05 10:08:44 -0500

    chore: metis andromeda chainId (#540)
    * chore: metis andromeda chainId
    
    * chore: ProtocolAction

--- packages/contract-helpers/src/commons/types.ts
+++ packages/contract-helpers/src/commons/types.ts
@@ -34,6 +34,7 @@ export const ChainIdToNetwork: Record<number, string> = {
   1666700000: 'harmony_testnet',
   11155111: 'sepolia',
   534353: 'scroll_alpha',
+  1088: 'metis_andromeda',
 };
 
 export enum ChainId {
@@ -60,6 +61,7 @@ export enum ChainId {
   zkevm_testnet = 1402,
   sepolia = 11155111,
   scroll_alpha = 534353,
+  metis_andromeda = 1088,
 }
 export type ConstantAddressesByNetwork = Record<
   string,
