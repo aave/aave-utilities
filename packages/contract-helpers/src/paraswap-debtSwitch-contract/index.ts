76f843a1c6e4f937e13974cc06e7278a044f269f
aschmidt20 <andrew@aave.com>
2023-07-05 23:37:13 -0500

    feat: construct tx data

--- packages/contract-helpers/src/paraswap-debtSwitch-contract/index.ts
+++ packages/contract-helpers/src/paraswap-debtSwitch-contract/index.ts
@@ -1,104 +1,98 @@
 import {
-    BigNumberish,
-    BytesLike,
-    PopulatedTransaction,
-    providers,
-    utils,
+  BigNumberish,
+  BytesLike,
+  PopulatedTransaction,
+  providers,
 } from 'ethers';
 import BaseService from '../commons/BaseService';
-import { InterestRate } from '../commons/types';
+import { augustusToAmountOffsetFromCalldata } from '../commons/utils';
 import {
-    ParaSwapDebtSwapAdapter,
-    ParaSwapDebtSwapAdapterInterface,
+  ParaSwapDebtSwapAdapter,
+  ParaSwapDebtSwapAdapterInterface,
 } from './typechain/ParaSwapDebtSwitchAdapter';
 import { ParaSwapDebtSwapAdapter__factory } from './typechain/ParaSwapDebtSwitchAdapter__factory';
 
 export type DebtSwitchType = {
-    user: string;
-    debtAsset: string;
-    debtRepayAmount: string;
-    debtRateMode: InterestRate;
-    newDebtAsset: string;
-    maxNewDebtAmount: string;
-    offset: number;
-    paraswapData: string;
-    deadline: BigNumberish;
-    sigV: BigNumberish;
-    sigR: BytesLike;
-    sigS: BytesLike;
+  user: string;
+  debtAsset: string;
+  debtRepayAmount: string;
+  debtRateMode: number;
+  newDebtAsset: string;
+  maxNewDebtAmount: string;
+  repayAll: boolean;
+  txCalldata: string;
+  deadline: BigNumberish;
+  sigV: BigNumberish;
+  sigR: BytesLike;
+  sigS: BytesLike;
 };
 
 export interface ParaswapDebtSwitchInterface {
-    debtSwitch: (args: DebtSwitchType) => PopulatedTransaction;
+  debtSwitch: (args: DebtSwitchType) => PopulatedTransaction;
 }
 
-export class ParaSwapDebtSwitch
-    extends BaseService<ParaSwapDebtSwapAdapter>
-    implements ParaswapDebtSwitchInterface {
-    readonly debtSwitchAddress: string;
-    readonly contractInterface: ParaSwapDebtSwapAdapterInterface;
+export class DebtSwitchAdapterService
+  extends BaseService<ParaSwapDebtSwapAdapter>
+  implements ParaswapDebtSwitchInterface
+{
+  readonly debtSwitchAddress: string;
+  readonly contractInterface: ParaSwapDebtSwapAdapterInterface;
 
-    constructor(provider: providers.Provider, debtSwitchAddress?: string) {
-        super(provider, ParaSwapDebtSwapAdapter__factory);
+  constructor(provider: providers.Provider, debtSwitchAddress?: string) {
+    super(provider, ParaSwapDebtSwapAdapter__factory);
 
-        this.debtSwitchAddress = debtSwitchAddress ?? '';
+    this.debtSwitchAddress = debtSwitchAddress ?? '';
 
-        this.contractInterface = ParaSwapDebtSwapAdapter__factory.createInterface();
+    this.contractInterface = ParaSwapDebtSwapAdapter__factory.createInterface();
 
-        this.debtSwitch = this.debtSwitch.bind(this);
-    }
+    this.debtSwitch = this.debtSwitch.bind(this);
+  }
 
-    public debtSwitch({
-        user,
-        debtAsset,
-        debtRepayAmount,
-        debtRateMode,
-        newDebtAsset,
-        maxNewDebtAmount,
-        offset,
-        paraswapData,
+  public debtSwitch({
+    user,
+    debtAsset,
+    debtRepayAmount,
+    debtRateMode,
+    newDebtAsset,
+    maxNewDebtAmount,
+    repayAll,
+    txCalldata,
+    deadline,
+    sigV,
+    sigR,
+    sigS,
+  }: DebtSwitchType): PopulatedTransaction {
+    const txParamsStruct: ParaSwapDebtSwapAdapter.DebtSwapParamsStruct = {
+      debtAsset,
+      debtRepayAmount,
+      debtRateMode,
+      newDebtAsset,
+      maxNewDebtAmount,
+      offset: repayAll ? augustusToAmountOffsetFromCalldata(txCalldata) : 0,
+      paraswapData: txCalldata,
+    };
+
+    const creditDelParamsStruct: ParaSwapDebtSwapAdapter.CreditDelegationInputStruct =
+      {
+        debtToken: newDebtAsset,
+        value: maxNewDebtAmount,
         deadline,
-        sigV,
-        sigR,
-        sigS,
-    }: DebtSwitchType): PopulatedTransaction {
-        const encodedParams = utils.defaultAbiCoder.encode(
-            [
-                'address',
-                'uint256',
-                'uint256',
-                'address',
-                'uint256',
-                'uint256',
-                'bytes',
-            ],
-            [
-                debtAsset,
-                debtRepayAmount,
-                debtRateMode,
-                newDebtAsset,
-                maxNewDebtAmount,
-                offset,
-                paraswapData,
-            ],
-        );
+        v: sigV,
+        r: sigR,
+        s: sigS,
+      };
 
-        const encodedCreditDelegationParams = utils.defaultAbiCoder.encode(
-            ['address', 'uint256', 'uint256', 'uint8', 'bytes32', 'bytes32'],
-            [debtAsset, debtRepayAmount, deadline, sigV, sigR, sigS],
-        );
+    const actionTx: PopulatedTransaction = {};
 
-        const actionTx: PopulatedTransaction = {};
+    const txData = this.contractInterface.encodeFunctionData('swapDebt', [
+      txParamsStruct,
+      creditDelParamsStruct,
+    ]);
 
-        const txData = this.contractInterface.encodeFunctionData('swapDebt', [
-            encodedParams,
-            encodedCreditDelegationParams,
-        ]);
+    actionTx.to = this.debtSwitchAddress;
+    actionTx.data = txData;
+    actionTx.from = user;
 
-        actionTx.to = this.debtSwitchAddress;
-        actionTx.data = txData;
-        actionTx.from = user;
-
-        return actionTx;
-    }
+    return actionTx;
+  }
 }
