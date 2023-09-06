9a9b6935eb38067043d6773ef5b53be01fc9b667
Mark Grothe <grothem@gmail.com>
2023-09-06 16:08:16 -0500

    fix: bad merge

--- packages/contract-helpers/src/commons/utils.ts
+++ packages/contract-helpers/src/commons/utils.ts
@@ -110,6 +110,10 @@ export const gasLimitRecommendations: GasRecommendationType = {
     limit: '350000',
     recommended: '350000',
   },
+  [ProtocolAction.stake]: {
+    limit: '350000',
+    recommended: '350000',
+  },
   [ProtocolAction.stakeWithPermit]: {
     limit: '400000',
     recommended: '400000',
