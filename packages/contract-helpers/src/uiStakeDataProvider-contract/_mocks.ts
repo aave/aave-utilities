8790777d06443c5fb571663866285b6e2e31627b
Mark Hinschberger <foodaka@users.noreply.github.com>
2023-12-01 11:56:57 +0000

    chore fix mocks stkGHO

--- packages/contract-helpers/src/uiStakeDataProvider-contract/_mocks.ts
+++ packages/contract-helpers/src/uiStakeDataProvider-contract/_mocks.ts
@@ -149,6 +149,82 @@ export const GeneralStakeUIDataRaw = {
       _isBigNumber: true,
     }),
   },
+
+  stkGHO: {
+    stakedTokenTotalSupply: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    stakedTokenTotalRedeemableAmount: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    stakeCooldownSeconds: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    stakeUnstakeWindow: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    stakedTokenPriceEth: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    rewardTokenPriceEth: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    stakeApy: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    distributionPerSecond: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    distributionEnd: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    0: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    1: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    2: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    3: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    4: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    5: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    6: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    7: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    8: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+  },
+
   ethPrice: BigNumber.from({
     _hex: '0x0',
     _isBigNumber: true,
@@ -410,6 +486,17 @@ export const GeneralStakeUIDataHumanized = {
     distributionPerSecond: '0',
     distributionEnd: '0',
   },
+  stkGHO: {
+    stakeTokenTotalSupply: '0',
+    stakeTokenTotalRedeemableAmount: '0',
+    stakeCooldownSeconds: 0,
+    stakeUnstakeWindow: 0,
+    stakeTokenPriceEth: '0',
+    rewardTokenPriceEth: '0',
+    stakeApy: '0',
+    distributionPerSecond: '0',
+    distributionEnd: '0',
+  },
   ethPriceUsd: '0',
 };
 
@@ -423,6 +510,14 @@ export const GetUserStakeUIDataHumanized = {
     userIncentivesToClaim: '0',
   },
   bpt: {
+    stakeTokenUserBalance: '0',
+    underlyingTokenUserBalance: '0',
+    stakeTokenRedeemableAmount: '0',
+    userCooldownTimestamp: 0,
+    userCooldownAmount: '0',
+    userIncentivesToClaim: '0',
+  },
+  stkGHO: {
     stakeTokenUserBalance: '0',
     underlyingTokenUserBalance: '0',
     stakeTokenRedeemableAmount: '0',
