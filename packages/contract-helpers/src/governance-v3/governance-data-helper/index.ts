644ea70fc21625a27584bf4f0c00132f10da2362
Mark Grothe <grothem@gmail.com>
2023-12-11 13:13:22 -0600

    fix: build

--- packages/contract-helpers/src/governance-v3/governance-data-helper/index.ts
+++ packages/contract-helpers/src/governance-v3/governance-data-helper/index.ts
@@ -1,7 +1,33 @@
 import { providers } from 'ethers';
 import { GovernanceDataHelper as GovernanceDataHelperContract } from '../typechain/GovernanceDataHelper';
 import { GovernanceDataHelper__factory } from '../typechain/factories/GovernanceDataHelper__factory';
-import { AccessLevel, ProposalV3State } from '../types';
+
+export enum AccessLevel {
+  /** Do not use */
+  None,
+  /** listing assets, changes of assets params, updates of the protocol etc */
+  Short_Executor,
+  /** payloads controller updates */
+  Long_Executor,
+}
+
+export enum ProposalV3State {
+  /** proposal does not exist */
+  Null,
+  /** created, waiting for a cooldown to initiate the balances snapshot */
+  Created,
+  /** balances snapshot set, voting in progress */
+  Active,
+  /** voting results submitted, but proposal is under grace period when guardian can cancel it */
+  Queued,
+  /** results sent to the execution chain(s) */
+  Executed,
+  /** voting was not successful */
+  Failed,
+  /** got cancelled by guardian, or because proposition power of creator dropped below allowed minimum */
+  Cancelled,
+  Expired,
+}
 
 export type ProposalPayload = {
   chain: number;
