7ec04b025cebd9f9f6eb0c1b7d4dd0960f354110
Mark Grothe <grothem@gmail.com>
2023-12-21 09:59:39 -0600

    feat: token delegate

--- packages/contract-helpers/src/governance-v3/aave-token-v3/index.ts
+++ packages/contract-helpers/src/governance-v3/aave-token-v3/index.ts
@@ -1,14 +1,16 @@
-import { providers } from 'ethers';
+import { BigNumber, PopulatedTransaction, providers } from 'ethers';
 import { AaveTokenV3 } from '../typechain/AaveTokenV3';
 import { AaveTokenV3__factory } from '../typechain/factories/AaveTokenV3__factory';
 
 export enum GovernancePowerType {
   VOTING,
   PROPOSITION,
+  ALL,
 }
 
 export class AaveTokenV3Service {
   readonly _contract: AaveTokenV3;
+  readonly _contractInterface = AaveTokenV3__factory.createInterface();
 
   constructor(tokenAddress: string, provider: providers.Provider) {
     this._contract = AaveTokenV3__factory.connect(tokenAddress, provider);
@@ -26,5 +28,30 @@ export class AaveTokenV3Service {
     return this._contract.functions.getPowerCurrent(user, delegationType, {
       blockTag: blockNumber,
     });
+  }
+
+  public getDelegateTxData(
+    user: string,
+    delegateTo: string,
+    type: GovernancePowerType,
+  ): PopulatedTransaction {
+    const tx: PopulatedTransaction = {};
+    if (type === GovernancePowerType.ALL) {
+      tx.data = this._contractInterface.encodeFunctionData('delegate', [
+        delegateTo,
+      ]);
+    } else {
+      tx.data = this._contractInterface.encodeFunctionData('delegateByType', [
+        delegateTo,
+        type,
+      ]);
+    }
+
+    return {
+      ...tx,
+      to: this._contract.address,
+      from: user,
+      gasLimit: BigNumber.from('1000000'),
+    };
   }
 }
