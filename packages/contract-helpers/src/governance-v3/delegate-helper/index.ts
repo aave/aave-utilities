5ba990b909ab36802903e4d5a893d050635e6117
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-01-02 16:52:03 +0000

    fix: sig func

--- packages/contract-helpers/src/governance-v3/delegate-helper/index.ts
+++ packages/contract-helpers/src/governance-v3/delegate-helper/index.ts
@@ -1,8 +1,8 @@
 import { SignatureLike } from '@ethersproject/bytes';
 import { BigNumber, PopulatedTransaction, Signature, utils } from 'ethers';
+import { tEthereumAddress, ENS } from '../../commons/types';
 import { MetaDelegateHelperInterface } from '../typechain/MetaDelegateHelper';
 import { MetaDelegateHelper__factory } from '../typechain/factories/MetaDelegateHelper__factory';
-
 export enum DelegationType {
   VOTING,
   PROPOSITION,
@@ -17,6 +17,27 @@ export type MetaDelegateParams = {
   deadline: string;
   signature: SignatureLike;
 };
+
+export type GovPrepareDelegateSigByType = {
+  delegatee: tEthereumAddress | ENS;
+  type: string;
+  nonce: string;
+  expiry: string;
+  governanceTokenName: string;
+  governanceToken: tEthereumAddress;
+};
+
+interface DelegateMetaSigParams {
+  underlyingAsset: tEthereumAddress;
+  delegatee: tEthereumAddress | ENS;
+  delegationType: DelegationType;
+  delegator: tEthereumAddress;
+  increaseNonce: boolean;
+  governanceTokenName: string;
+  nonce: string;
+  connectedChainId: number;
+  deadline: string;
+}
 
 export class MetaDelegateHelperService {
   private readonly _contractInterface: MetaDelegateHelperInterface;
@@ -48,5 +69,92 @@ export class MetaDelegateHelperService {
       gasLimit: BigNumber.from('1000000'),
     };
     return tx;
+  }
+
+  // @GovDelegationValidator
+  public async prepareV3DelegateByTypeSignature(
+    // @isEthAddressOrENS('delegatee')
+    // @isEthAddress('governanceToken')
+    // @is0OrPositiveAmount('nonce')
+    {
+      underlyingAsset,
+      delegatee,
+      delegationType,
+      delegator,
+      increaseNonce,
+      governanceTokenName,
+      nonce,
+      connectedChainId,
+      deadline,
+    }: DelegateMetaSigParams,
+  ): Promise<string> {
+    const isAllDelegate = Number(delegationType) === Number(DelegationType.ALL);
+
+    const sigBaseType = [
+      { name: 'nonce', type: 'uint256' },
+      { name: 'deadline', type: 'uint256' },
+    ];
+    const sigParametersType = [
+      { name: 'delegator', type: 'address' },
+      { name: 'delegatee', type: 'address' },
+    ];
+    const sigDelegationTypeType = [{ name: 'delegationType', type: 'uint8' }];
+
+    const typesData = {
+      delegator,
+      delegatee,
+      nonce: BigInt(increaseNonce ? Number(nonce) + 1 : nonce).toString(),
+      deadline,
+    };
+
+    const eIP712DomainType = {
+      EIP712Domain: [
+        {
+          name: 'name',
+          type: 'string',
+        },
+        {
+          name: 'version',
+          type: 'string',
+        },
+        {
+          name: 'chainId',
+          type: 'uint256',
+        },
+        {
+          name: 'verifyingContract',
+          type: 'address',
+        },
+      ],
+    };
+
+    const typeData = {
+      domain: {
+        name: governanceTokenName,
+        version: '2',
+        chainId: connectedChainId,
+        verifyingContract: underlyingAsset,
+      },
+      types: isAllDelegate
+        ? {
+            ...eIP712DomainType,
+            Delegate: [...sigParametersType, ...sigBaseType],
+          }
+        : {
+            ...eIP712DomainType,
+
+            DelegateByType: [
+              ...sigParametersType,
+              ...sigDelegationTypeType,
+              ...sigBaseType,
+            ],
+          },
+      primaryType: isAllDelegate ? 'Delegate' : 'DelegateByType',
+      message: isAllDelegate
+        ? { ...typesData }
+        : { ...typesData, delegationType },
+    };
+
+    return JSON.stringify(typeData);
   }
 }
