817cc8d9840fb126075cddb7a26156277c668cf0
Mark Hinschberger <foodaka@users.noreply.github.com>
2024-08-26 06:24:48 -0400

    Feat/decimals apr (#590)
    * fix: decimals
    
    * fix: dec

--- packages/math-utils/src/formatters/incentive/calculate-incentive-apr.ts
+++ packages/math-utils/src/formatters/incentive/calculate-incentive-apr.ts
@@ -1,5 +1,5 @@
 import { normalize, normalizeBN, valueToBigNumber } from '../../bignumber';
-import { SECONDS_PER_YEAR, WEI_DECIMALS } from '../../constants';
+import { SECONDS_PER_YEAR } from '../../constants';
 
 export interface CalculateIncentiveAPRRequest {
   emissionPerSecond: string;
@@ -20,7 +20,7 @@ export function calculateIncentiveAPR({
 }: CalculateIncentiveAPRRequest): string {
   const emissionPerSecondNormalized = normalizeBN(
     emissionPerSecond,
-    WEI_DECIMALS,
+    decimals,
   ).multipliedBy(rewardTokenPriceInMarketReferenceCurrency);
 
   if (emissionPerSecondNormalized.eq(0)) {
