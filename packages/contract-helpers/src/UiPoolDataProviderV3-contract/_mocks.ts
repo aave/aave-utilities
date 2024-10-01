1edf6e749633ce437a480eea12f15498c08bc8f7
Joaquin Battilana <battilanajn@gmail.com>
2024-10-01 14:46:22 +0100

    feat: basic getEModes and provider v3 implementation

---
+++ packages/contract-helpers/src/UiPoolDataProviderV3-contract/_mocks.ts
@@ -0,0 +1,291 @@
+import { BigNumber } from 'ethers';
+import { EModeData, ReservesData, UserReserveData } from './types';
+
+export const reservesMock: ReservesData = {
+  0: [
+    {
+      underlyingAsset: '0x3E0437898a5667a4769B1Ca5A34aAB1ae7E81377',
+      name: '',
+      symbol: 'AMPL',
+      decimals: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      baseLTVasCollateral: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationThreshold: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationBonus: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveFactor: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      usageAsCollateralEnabled: false,
+      borrowingEnabled: true,
+      isActive: true,
+      isFrozen: false,
+      isPaused: false,
+      isSiloedBorrowing: false,
+      liquidityIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      liquidityRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      lastUpdateTimestamp: 1631772892,
+      aTokenAddress: '0xb8a16bbab34FA7A5C09Ec7679EAfb8fEC06897bc',
+      variableDebtTokenAddress: '0xb7b7AF565495670713C92B8848fC8A650a968F81',
+      interestRateStrategyAddress: '0x796ec26fc7df8D81BCB5BABF74ccdE0E2B122164',
+      availableLiquidity: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      totalScaledVariableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      priceInMarketReferenceCurrency: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      priceOracle: '0x796ec26fc7df8D81BCB5BABF74ccdE0E2B122164',
+      variableRateSlope1: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableRateSlope2: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      baseVariableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      optimalUsageRatio: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      // new
+      debtCeiling: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      borrowCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      supplyCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      borrowableInIsolation: false,
+      flashLoanEnabled: false,
+      accruedToTreasury: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      unbacked: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      isolationModeTotalDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      debtCeilingDecimals: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      virtualAccActive: false,
+      virtualUnderlyingBalance: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+    },
+    {
+      underlyingAsset: '0xA478c2975Ab1Ea89e8196811F51A7B7Ade33eB11',
+      name: '',
+      symbol: 'UNI-V2',
+      decimals: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      baseLTVasCollateral: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationThreshold: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveLiquidationBonus: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      reserveFactor: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      usageAsCollateralEnabled: false,
+      borrowingEnabled: true,
+      isActive: true,
+      isFrozen: false,
+      isPaused: false,
+      isSiloedBorrowing: false,
+      liquidityIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowIndex: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      liquidityRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      lastUpdateTimestamp: 1631772892,
+      aTokenAddress: '0xb8a16bbab34FA7A5C09Ec7679EAfb8fEC06897bc',
+      variableDebtTokenAddress: '0xb7b7AF565495670713C92B8848fC8A650a968F81',
+      interestRateStrategyAddress: '0x796ec26fc7df8D81BCB5BABF74ccdE0E2B122164',
+      availableLiquidity: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      totalScaledVariableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      priceInMarketReferenceCurrency: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      priceOracle: '0x796ec26fc7df8D81BCB5BABF74ccdE0E2B122164',
+      variableRateSlope1: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      variableRateSlope2: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      baseVariableBorrowRate: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      optimalUsageRatio: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      // new
+      debtCeiling: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      borrowCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      supplyCap: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      borrowableInIsolation: false,
+      flashLoanEnabled: false,
+      accruedToTreasury: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      unbacked: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      isolationModeTotalDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      debtCeilingDecimals: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      virtualAccActive: false,
+      virtualUnderlyingBalance: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+    },
+  ],
+  1: {
+    marketReferenceCurrencyUnit: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    marketReferenceCurrencyPriceInUsd: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+
+    networkBaseTokenPriceInUsd: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    networkBaseTokenPriceDecimals: 0,
+  },
+};
+
+export const userReservesMock: UserReserveData = {
+  0: [
+    {
+      underlyingAsset: '0xB597cd8D3217ea6477232F9217fa70837ff667Af',
+      scaledATokenBalance: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+      usageAsCollateralEnabledOnUser: false,
+      scaledVariableDebt: BigNumber.from({
+        _hex: '0x0',
+        _isBigNumber: true,
+      }),
+    },
+  ],
+  1: 1,
+};
+
+export const eModesMock: EModeData = {
+  id: 1,
+  eMode: {
+    ltv: 1,
+    liquidationThreshold: 1,
+    liquidationBonus: 1,
+    label: 'test label',
+    collateralBitmap: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+    borrowableBitmap: BigNumber.from({
+      _hex: '0x0',
+      _isBigNumber: true,
+    }),
+  },
+};
---
+++ packages/contract-helpers/src/UiPoolDataProviderV3-contract/types.ts
@@ -0,0 +1,143 @@
+import { BigNumber } from 'ethers';
+
+export interface EModeCategory {
+  ltv: number;
+  liquidationThreshold: number;
+  liquidationBonus: number;
+  collateralBitmap: BigNumber;
+  label: string;
+  borrowableBitmap: BigNumber;
+}
+
+export interface EModeData {
+  id: number;
+  eMode: EModeCategory;
+}
+
+export interface ReservesData {
+  0: Array<{
+    underlyingAsset: string;
+    name: string;
+    symbol: string;
+    decimals: BigNumber;
+    baseLTVasCollateral: BigNumber;
+    reserveLiquidationThreshold: BigNumber;
+    reserveLiquidationBonus: BigNumber;
+    reserveFactor: BigNumber;
+    usageAsCollateralEnabled: boolean;
+    borrowingEnabled: boolean;
+    isActive: boolean;
+    isFrozen: boolean;
+    liquidityIndex: BigNumber;
+    variableBorrowIndex: BigNumber;
+    liquidityRate: BigNumber;
+    variableBorrowRate: BigNumber;
+    lastUpdateTimestamp: number;
+    aTokenAddress: string;
+    variableDebtTokenAddress: string;
+    interestRateStrategyAddress: string;
+    availableLiquidity: BigNumber;
+    totalScaledVariableDebt: BigNumber;
+    priceInMarketReferenceCurrency: BigNumber;
+    priceOracle: string;
+    variableRateSlope1: BigNumber;
+    variableRateSlope2: BigNumber;
+    baseVariableBorrowRate: BigNumber;
+    optimalUsageRatio: BigNumber;
+    isPaused: boolean;
+    isSiloedBorrowing: boolean;
+    accruedToTreasury: BigNumber;
+    unbacked: BigNumber;
+    isolationModeTotalDebt: BigNumber;
+    flashLoanEnabled: boolean;
+    debtCeiling: BigNumber;
+    debtCeilingDecimals: BigNumber;
+    borrowCap: BigNumber;
+    supplyCap: BigNumber;
+    borrowableInIsolation: boolean;
+    virtualAccActive: boolean;
+    virtualUnderlyingBalance: BigNumber;
+  }>;
+  1: {
+    marketReferenceCurrencyUnit: BigNumber;
+    marketReferenceCurrencyPriceInUsd: BigNumber;
+    networkBaseTokenPriceInUsd: BigNumber;
+    networkBaseTokenPriceDecimals: number;
+  };
+}
+
+export interface UserReserveData {
+  0: Array<{
+    underlyingAsset: string;
+    scaledATokenBalance: BigNumber;
+    usageAsCollateralEnabledOnUser: boolean;
+    scaledVariableDebt: BigNumber;
+  }>;
+  1: number;
+}
+
+export interface PoolBaseCurrencyHumanized {
+  marketReferenceCurrencyDecimals: number;
+  marketReferenceCurrencyPriceInUsd: string;
+  networkBaseTokenPriceInUsd: string;
+  networkBaseTokenPriceDecimals: number;
+}
+
+export interface ReserveDataHumanized {
+  id: string;
+  underlyingAsset: string;
+  name: string;
+  symbol: string;
+  decimals: number;
+  baseLTVasCollateral: string;
+  reserveLiquidationThreshold: string;
+  reserveLiquidationBonus: string;
+  reserveFactor: string;
+  usageAsCollateralEnabled: boolean;
+  borrowingEnabled: boolean;
+  isActive: boolean;
+  isFrozen: boolean;
+  liquidityIndex: string;
+  variableBorrowIndex: string;
+  liquidityRate: string;
+  variableBorrowRate: string;
+  lastUpdateTimestamp: number;
+  aTokenAddress: string;
+  variableDebtTokenAddress: string;
+  interestRateStrategyAddress: string;
+  availableLiquidity: string;
+  totalScaledVariableDebt: string;
+  priceInMarketReferenceCurrency: string;
+  priceOracle: string;
+  variableRateSlope1: string;
+  variableRateSlope2: string;
+  baseVariableBorrowRate: string;
+  optimalUsageRatio: string;
+  // v3 only
+  isPaused: boolean;
+  isSiloedBorrowing: boolean;
+  accruedToTreasury: string;
+  unbacked: string;
+  isolationModeTotalDebt: string;
+  flashLoanEnabled: boolean;
+  debtCeiling: string;
+  debtCeilingDecimals: number;
+  borrowCap: string;
+  supplyCap: string;
+  borrowableInIsolation: boolean;
+  virtualAccActive: boolean;
+  virtualUnderlyingBalance: string;
+}
+
+export interface ReservesDataHumanized {
+  reservesData: ReserveDataHumanized[];
+  baseCurrencyData: PoolBaseCurrencyHumanized;
+}
+
+export interface UserReserveDataHumanized {
+  id: string;
+  underlyingAsset: string;
+  scaledATokenBalance: string;
+  usageAsCollateralEnabledOnUser: boolean;
+  scaledVariableDebt: string;
+}
