bdb3b8849551acb03bc85f5dda841b2b5c43509b
Joaquin Battilana <battilanajn@gmail.com>
2024-10-08 17:53:44 +0100

    feat: added new token gateway types

--- packages/contract-types/src/types/factories/index.ts
+++ packages/contract-types/src/types/factories/index.ts
@@ -2,3 +2,4 @@
 /* eslint-disable */
 export { UiIncentiveDataProviderV3__factory } from './UiIncentiveDataProviderV3__factory';
 export { UiPoolDataProviderV3__factory } from './UiPoolDataProviderV3__factory';
+export { WrappedTokenGatewayV3__factory } from './WrappedTokenGatewayV3__factory';
