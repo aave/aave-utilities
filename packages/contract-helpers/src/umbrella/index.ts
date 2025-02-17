8cfd2302b3da92d7db6559cc9dee24665e7806bb
Mark Grothe <grothem@gmail.com>
2025-02-17 16:21:07 -0600

    fix: export

--- packages/contract-helpers/src/umbrella/index.ts
+++ packages/contract-helpers/src/umbrella/index.ts
@@ -1,4 +1,4 @@
 export { StakeTokenService } from './StakeToken';
 export { RewardsDistributorService } from './RewardsDistributor';
 export { StakeGatewayService } from './StakeGateway';
-export { StakeDataProviderService } from './StakeDataProvider';
+export * from './StakeDataProvider';
